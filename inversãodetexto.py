5363bd15c562928a7c07e1e0e457a7ceb0195a65
Jonathan <jonathan.silveira@outlook.com>
2021-11-21 00:13:48 -0300

    Aprendizado

--- inversãodetexto.py
+++ inversãodetexto.py
@@ -1,2 +1,11 @@
-txt = ('Jonathan da Costa Silveira')[::-1]
-print(txt)
+while True:
+    
+    txt = str(input('\033[0;32m\nDigite o texto: \033[m'))[::-1]
+    print(txt)   
+    resp = " "
+    while resp not in "10":
+        resp = str(input("\nDeseja continuar [1 - SIM / 0 - NÃO]? ")).strip().upper()[0]
+    if resp == "0":
+        break
+    
+print("\033[0;36;1;4m\nVocê optou por finalizar!\033[m\n")
