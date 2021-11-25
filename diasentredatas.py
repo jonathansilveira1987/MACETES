367647aac7ed86dd52148b1c36785bd119ce858d
Jonathan <jonathan.silveira@outlook.com>
2021-11-25 20:37:45 -0300

    Aprendizado

--- diasentredatas.py
+++ diasentredatas.py
@@ -2,11 +2,11 @@ from datetime import date
 
 # 10/10/2021 - 02/10/1987
 
-ano = int(input('Ano de Nascimento? '))
+ano = int(input('\nAno de Nascimento? '))
 mes = int(input('Mês de Nascimento? '))
 dia = int(input('Dia de Nascimento? '))
 
 dias = date.today() - date(ano, mes, dia)
 
-print(f'Já se passaram {dias.days} dias desde seu nascimento.')
+print(f'\nJá se passaram \033[0;34m{dias.days}\033[m dias desde seu nascimento.\n')
 # 12427 days
