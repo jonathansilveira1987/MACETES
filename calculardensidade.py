d7afeeb26c21481c0affcfef02cdcaded8fdc1d0
Jonathan <jonathan.silveira@outlook.com>
2021-11-24 22:26:46 -0300

    Aprendizado

---
+++ calculardensidade.py
@@ -0,0 +1,14 @@
+# cm³.
+
+# Densidade Absoluta ou Massa Específica.
+# Medimos essa grandeza medida em gramas = g/cm³
+# densidade = g/cm³
+# massa = gramas
+# volume = cm³
+# Fórmula: d = m/v
+
+# Massa em KG.
+d = float(input('Densidade: '))
+v = float(input('Volume: '))
+m = ((d * v) / 1000)
+print(f'A densidade é: {m} KG')
