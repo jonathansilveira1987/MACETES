324a747fad186768cb0eed7b5865770f1505777d
Jonathan <jonathan.silveira@outlook.com>
2021-11-26 22:47:28 -0300

    Aprendizado

--- casas_decimais.py
+++ casas_decimais.py
@@ -5,4 +5,14 @@
 # print("%.2f" % numero2) // 4.39
 
 teste = 7.98562434234
+
+print('O valor de teste formatado é {:.1f}'.format(teste))
+print('O valor de teste formatado é {:.2f}'.format(teste))
+print('O valor de teste formatado é {:.3f}'.format(teste))
 print('O valor de teste formatado é {:.4f}'.format(teste))
+print('O valor de teste formatado é {:.5f}'.format(teste))
+print('O valor de teste formatado é {:.6f}'.format(teste))
+print('O valor de teste formatado é {:.7f}'.format(teste))
+print('O valor de teste formatado é {:.8f}'.format(teste))
+print('O valor de teste formatado é {:.9f}'.format(teste))
+print('O valor de teste formatado é {:.10f}'.format(teste))
