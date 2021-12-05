99b2b3b75599bc8760246046f50f767049b4389a
Jonathan <jonathan.silveira@outlook.com>
2021-12-05 15:52:03 -0300

    Aprendizado

---
+++ a001tester.py
@@ -0,0 +1,40 @@
+# Calendário.
+# Desenvolvido por Jonathan Silveira - Instagram: @ jonathandev01
+
+import calendar
+from datetime import date
+
+while True:
+    print("\n\033[0;33mDigite 0 para imprimir o calendário do ano atual ou informe o ano desejado.\033[m\n")
+    ano = int(input("\033[0;32m> "))
+    print('\033[m')
+
+
+    # Aqui vai o "Tente novamente!"
+    if ano == int(ano):
+        break
+    print("\n\033[0;31mValor incorreto, tente novamente.\033[m\n", end=" ")
+    print("\n\033[0;33mDigite 0 para imprimir o calendário do ano atual ou informe o ano desejado.\033[m\n")
+    ano = int(input("\033[0;32m> "))
+    print('\033[m')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    if ano == 0:
+        ano_atual = date.today().year
+        print(calendar.calendar(ano_atual))   
+    else:
+        print(calendar.calendar(ano))
+    resp = " "
+    while resp not in "10":
+        resp = str(input("\nDeseja continuar [1 - SIM / 0 - NÃO]? ")).strip().upper()[0]
+    if resp == "0":
+        break    
+print("\033[0;36;1;4m\nVocê optou por finalizar!\033[m\n")
