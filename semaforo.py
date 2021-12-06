e2d79bb83312763177f2a93801d06563d19b76d4
Jonathan <jonathan.silveira@outlook.com>
2021-12-05 21:32:41 -0300

    Aprendizado

--- semaforo.py
+++ semaforo.py
@@ -1,61 +1,3 @@
-
-
-
-
-
-while True:
-        # Programa Principal!
-        cor = input('\nCor: ')
-        if cor == 'verde':
-            print('\n\033[0;32mAcelerar!\033[m')
-        if cor == 'amarelo':
-            print('\n\033[0;33mAtenção!\033[m')
-        if cor == 'vermelho':
-            print('\n\033[0;31mPare!\033[m')
-            # Aqui vai o "Tente novamente!"
-            if cor != 'verde' 'amarelo' 'vermelho':
-                break
-            print("\n\033[0;31mValor incorreto, tente novamente.\033[m\n", end=" ")
-                # Aqui vai o "Deseja continuar?"
-        resp = " "
-        while resp not in "10":
-            resp = str(input("\n\033[0;33mDeseja continuar [1 - SIM / 0 - NÃO]? \033[m")).strip().upper()[0]
-        if resp == "0":
-            break    
-print("\033[0;36;1;4m\nVocê optou por finalizar!\033[m\n")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-
 # Semáforo.
 
 # Programa principal!
@@ -64,22 +6,8 @@ print('\nSemáforo.')
 cor = input('\nCor: ')
 
 if cor == 'verde':
-    print('\n\033[0;32mAcelerar\033[m')
+    print('\n\033[0;32mAcelerar!\033[m\n')
 elif cor == 'amarelo':
-    print('\n\033[0;33mAtenção\033[m')
+    print('\n\033[0;33mAtenção!\033[m\n')
 else:
-    print('\n\033[0;31mParar\033[m')
-
-'''
-
-
-
-
-
-
-
-
-
-
-
-
+    print('\n\033[0;31mParar!\033[m\n')
