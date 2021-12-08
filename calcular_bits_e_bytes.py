ae5f42c5a2e455cb552872314bc9327abe1404b1
Jonathan <jonathan.silveira@outlook.com>
2021-12-08 20:13:14 -0300

    Aprendizado

--- calcular_bits_e_bytes.py
+++ calcular_bits_e_bytes.py
@@ -1,7 +1,10 @@
+# Calcular bits e bytes.
+
 from time import sleep
 
 while True:
-    
+
+    # Programa principal!
     print('''\nBIT é a menor unidade de medida computacional!
     1 BIT equivale a 1 caractere.\n
     Escolha uma unidade abaixo para saber quantos bits a mesma possui...\n 
@@ -25,7 +28,7 @@ while True:
         sleep(3)
         print("\033[0;32m\n1 Byte possui {} bits.\033[m\n".format(byte))
     # KiloByte.
-    if unidade == 2:
+    elif unidade == 2:
         kilobyte = 1 * 1024
         bit = (2 ** 10) * 8
         print('Calculando...')
@@ -38,7 +41,7 @@ while True:
         sleep(3)
         print("\033[0;31m8 mil .192 bits.\033[m\n")
     # MegaByte.
-    if unidade == 3:
+    elif unidade == 3:
         megabyte = 1 * 1024
         byte = 2 ** 20
         bit = (2 ** 20) * 8
@@ -54,7 +57,7 @@ while True:
         sleep(3)
         print("\033[0;31m8 milhões .388 mil .608 bits.\033[m\n")
     # GigaByte.
-    if unidade == 4:
+    elif unidade == 4:
         gigabyte = 1 * 1024
         byte = 2 ** 30
         bit = (2 ** 30) * 8
@@ -70,7 +73,7 @@ while True:
         sleep(3)
         print("\033[0;31m8 bilhões .589 milhões .934 mil .592 bits.\033[m\n")
     # TeraByte.
-    if unidade == 5:
+    elif unidade == 5:
         terabyte = 1 * 1024
         byte = 2 ** 40
         bit = (2 ** 40) * 8
@@ -86,7 +89,7 @@ while True:
         sleep(3)
         print("\033[0;31m8 trilhões .796 bilhões .093 milhões .022 mil .208 bits.\033[m\n")
     # PetaByte.
-    if unidade == 6:
+    elif unidade == 6:
         petabyte = 1 * 1024
         byte = 2 ** 50
         bit = (2 ** 50) * 8
@@ -102,7 +105,7 @@ while True:
         sleep(3)
         print("\033[0;31m9 quatrilhões .007 trilhões .199 bilhões .254 milhões .740 mil .992 bits.\033[m\n")
     # 7. ExaByte
-    if unidade == 7:
+    elif unidade == 7:
         exabyte = 1 * 1024
         byte = 2 ** 60
         bit = (2 ** 60) * 8
@@ -118,7 +121,7 @@ while True:
         sleep(3)
         print("\033[0;31m9 quintilhões .223 quatrilhões .372 trilhões .036 bilhões .854 milhões .775 mil .808 bits.\033[m\n")
     # 8. ZettaByte
-    if unidade == 8:
+    elif unidade == 8:
         zettabyte = 1 * 1024
         byte = 2 ** 70
         bit = (2 ** 70) * 8
@@ -134,7 +137,7 @@ while True:
         sleep(3)
         print("\033[0;31m9 sextilhões .444 quintilhões .732 quatrilhões .965 trilhões .739 bilhões .290 milhões .427 mil .392 bits.\033[m\n")
     # 9. YottaByte
-    if unidade == 9:
+    elif unidade == 9:
         yottabyte = 1 * 1024
         byte = 2 ** 80
         bit = (2 ** 80) * 8
@@ -149,11 +152,15 @@ while True:
         print("Abaixo segue número lido por extenso...")
         sleep(3)
         print("\033[0;31m9 septilhões .671 sextilhões .406 quintilhões .556 quatrilhões .917 trilhões .033 bilhões .397 milhôes .649 mil .408 bits.\033[m\n")
-    
+    else:
+        # Aqui vai o "Tente novamente!"
+        unidade != '1, 2, 3, 4, 5, 6, 7, 8, 9'
+        print("\n\033[0;31mValor incorreto, tente novamente.\033[m\n", end=" ")
+        continue
+    # Aqui vai o "Deseja continuar?"
     resp = " "
     while resp not in "10":
-        resp = str(input("\nDeseja continuar [1 - SIM / 0 - NÃO]? ")).strip().upper()[0]
+        resp = str(input("\n\033[0;33mDeseja continuar [1 - SIM / 0 - NÃO]? \033[m")).strip().upper()[0]
     if resp == "0":
         break
-    
 print("\033[0;36;1;4m\nVocê optou por finalizar!\033[m\n")
--- modelo.py
+++ modelo.py
@@ -1,6 +1,7 @@
-# Semáforo.
+# Modelo.
 
 while True:
+
     # Programa principal!
     print('\nSemáforo.')
     cor = input('\nCor: ')
@@ -10,6 +11,15 @@ while True:
                 print('\n\033[0;33mAtenção!\033[m\n')
     elif cor == 'vermelho':
                 print('\n\033[0;31mPare!\033[m\n')
+    
+    
+    
+    
+    
+    
+    
+    
+    
     else:
         # Aqui vai o "Tente novamente!"
         cor != 'verde' 'amarelo' 'vermelho'
@@ -22,82 +32,3 @@ while True:
     if resp == "0":
         break    
 print("\033[0;36;1;4m\nVocê optou por finalizar!\033[m\n")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-
-# Programa principal!
-print('\nSemáforo.')
-
-cor = input('\nCor: ')
-
-if cor == 'verde':
-    print('\n\033[0;32mAcelerar!\033[m\n')
-elif cor == 'amarelo':
-    print('\n\033[0;33mAtenção!\033[m\n')
-else:
-    print('\n\033[0;31mParar!\033[m\n')
-
-'''
