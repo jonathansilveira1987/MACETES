d519084d8188275715349d5a335aeb75a720866e
Jonathan <jonathan.silveira@outlook.com>
2021-12-19 11:44:18 -0300

    Aprendizado

--- maior&menor.py
+++ maior&menor.py
@@ -1,29 +1,25 @@
-# Maior e Menor valores v1.0.
-
+# Maior e Menor valor v1.0.
+# Programa principal.
 a = int(input("\n1º valor: "))
 b = int(input("2º valor: "))
 c = int(input("3º valor: "))
-
 # Verificando quem é o menor.
 menor = a
 if b < a and b < c:
     menor = b
 if c < a and c < b:
     menor = c
-
 # Verificando que é o maior.
 maior = a
 if b > a and b > c:
     maior = b
 if c > a and c > b:
     maior = c
-
 print("\nO menor valor digitado foi \033[0;32m{}\033[m.".format(menor))
 print("\nO maior valor digitado foi \033[0;33m{}\033[m.\n".format(maior))
 
 
-# Maior e Menor valores v2.0.
-
+# Maior e Menor valor v2.0.
 resp = "S"
 soma = quant = media = maior = menor = 0
 while resp in "Ss":
@@ -43,15 +39,29 @@ print("\nVocê digitou {} números e a média foi {}.".format(quant, media))
 print("\nO maior valor foi {} e o menor valor foi {}.\n".format(maior, menor))
 
 
-# Maior e menor valores em Tupla.
-
+# Maior e Menor valor em Tupla v3.0.
 from random import randint
-
 numeros = (randint(1, 10), randint(1, 10), 
     randint(1, 10), randint(1, 10), randint(1, 10))
-
 print(f"\nOs valores sorteados foram: ", end="")
 for n in numeros:
     print(f"{n} ", end="")
 print(f"\nO maior valor sorteado foi {max(numeros)}.")
 print(f"O menor valor sorteado foi {min(numeros)}.\n")
+
+
+# Maior e menor valor da sequência v4.0.
+maior = 0
+menor = 0
+for pessoas in range(1, 5):
+    peso = float(input("Peso da {}ª pessoa: ".format(pessoas)))
+    if pessoas == 1:
+        maior = peso
+        menor = peso
+    else: # Teste de possibilidades.
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print("\nO maior peso lido foi {} kg.".format(maior))
+print("O menor peso lido foi {} kg.\n".format(menor))
