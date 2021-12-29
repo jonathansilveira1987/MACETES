d943c72cd8a7c0199efbaa3d74f4d86ce65fb881
Jonathan <jonathan.silveira@outlook.com>
2021-12-29 16:43:02 -0300

    Aprendizado

--- funcaodescobremaior.py
+++ funcaodescobremaior.py
@@ -8,7 +8,7 @@ def maior(* num):
     print('Analisando os valores passados...')
     for valor in num:
         print(f'{valor}', end=' ', flush=True)
-        sleep(0.3)
+        sleep(0.8)
         if cont == 0:
             maior = valor
         else:
@@ -16,7 +16,7 @@ def maior(* num):
                 maior = valor
         cont = cont + 1
     print(f'Foram informados {cont} valores ao todo.')
-    print(f'O maior valor informado foi {maior}.')
+    print(f'O maior valor informado foi {maior}.\n')
 
 # Programa principal
 maior(2, 9, 4, 5, 7, 1)
