4b10d0cc0adee1672e09674397ed338f119d2c7d
Jonathan <jonathan.silveira@outlook.com>
2021-12-30 17:03:47 -0300

    Aprendizado

--- loopinfinito.py
+++ loopinfinito.py
@@ -4,3 +4,15 @@ mundo4 = False
 print('Obrigado pelo curso e pelo incrível trabalho')
 while not mundo4:
     print('No aguardo do mundo 4 :D')
+
+
+
+
+print(1)
+print(2)
+print(3)
+print(4)
+n = 1
+while True:
+    print(n)
+    n = n + 1
--- prototipo.py
+++ prototipo.py
@@ -1,8 +1,24 @@
-print(1)
-print(2)
-print(3)
-print(4)
-n = 1
-while True:
-    print(n)
-    n = n + 1
+import math
+
+# Raíz quadrada.
+num = float(input('Número: '))
+raiz = math.sqrt(num)
+print('Resultado = ', raiz)
+
+
+
+# Bhaskara.
+
+a = float(input('Delta 1: '))
+b = float(input('Delta 2: '))
+c = float(input('Delta 3: '))
+delta = b * b - 4 * a * c
+if delta < 0:
+    print("Não há raízes reais")
+elif delta == 0:
+    print("Há uma raiz distinta apenas")
+else:
+    print("Há duas raízes distintas")
+x1 = (-b + math.sqrt(delta)) / (2 * a)
+x2 = (-b - math.sqrt(delta)) / (2 * a)
+print(f"As raízes são {x1} e {x2}")
