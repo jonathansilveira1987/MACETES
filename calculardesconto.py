408519bc72adad30dc748abbb03f3e9ea6202719
Jonathan <jonathan.silveira@outlook.com>
2022-01-05 14:58:52 -0300

    Aprendizado

--- calculardesconto.py
+++
@@ -1,8 +0,0 @@
-# Calcular desconto.
-
-preco = float(input("\nQual é o preço do produto: R$ "))
-desc = float(input('Desconto % : '))
-novo = preco - (preco * desc / 100)
-valor_desconto = preco - novo
-print("\nO produto que custava R$ {:.2f}, na promoção com desconto de {:.0f}% vai custar R$ {:.2f}.".format(preco, desc, novo))
-print("Você obteve um desconto de R$ {:.2f}.\n".format(valor_desconto))
