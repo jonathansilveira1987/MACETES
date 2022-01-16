5c9f2febcefa4cfc326351bb9648c33b71f3395b
Jonathan <jonathan.silveira@outlook.com>
2022-01-16 12:30:46 -0300

    Aprendizado

--- utilidades.py
+++ utilidades.py
@@ -31,7 +31,7 @@ while True:
     [ 17 ] - 
     [ 18 ] - Adivinhar número
     [ 19 ] - 
-    [ 20 ] - 
+    [ 20 ] - Trabalhar com texto
     ''')
 
     opcao = input("Informe sua escolha desejada (0 para encerrar): ")
@@ -370,11 +370,58 @@ Loading…
         print('Disponibilizando ferramenta, por favor aguarde...')
         sleep(2)
         print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
-    # 
+    # Trabalhar com texto.
     elif opcao == '20':
         print('Disponibilizando ferramenta, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        # Criar arquivo TXT.
+        arquivo = open('bancodedados.txt', 'a')
+        file = input('Digite algo: ')
+        arquivo.write(file)
+        # Lendo o arquivo criado:
+        arquivo = open('bancodedados.txt','r')
+        for linha in arquivo:
+            linha = linha.rstrip()
+            print (linha)
+        arquivo.close()
+        # Acrescentando texto ao arquivo criado, usando o modo de acesso 'a'
+        print("\n")
+        texto = input("Digite uma frase para acrescentar ao arquivo:\n")
+        arquivo = open('arq01.txt','a')
+        arquivo.write(texto + "\n")
+        print("Opera��o conclu�da no arquivo " + arquivo.name + " usando o modo de acesso " + arquivo.mode)
+        arquivo.close()
+        print("\nTexto alterado:")
+        arquivo = open('arq01.txt','r')
+        for linha in arquivo:
+            linha = linha.rstrip()
+            print (linha)
+        arquivo.close()
+        # Acrescentando texto ao in�cio do arquivo, usando o modo 'r+'
+        print("\n")
+        texto = input("Digite um titulo para acrescentar ao arquivo:\n")
+        arquivo = open('arq01.txt','r+')
+        arquivo.seek(0)
+        arquivo.write(texto + '\n')
+        arquivo.close()
+        print("\nTexto alterado:")
+        arquivo = open('arq01.txt','r')
+        for linha in arquivo:
+            linha = linha.rstrip()
+            print (linha)
+        arquivo.close()
+        arquivo = open("bancodedados.txt", "a")
+        file = input('Digite algo: ')
+        arquivo.write(file)
+        # solution...?
+        arquivo = open("texto.txt", "a")
+        frases = list()
+        frases.append("TreinaWeb \n")
+        frases.append("Python \n")
+        frases.append("Arquivos \n")
+        frases.append("Django \n")
+        arquivo.writelines(frases)
+
 
 
 
