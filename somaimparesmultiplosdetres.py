716a1d2eba2234362518b0af74e4712e638c7942
Jonathan <jonathan.silveira@outlook.com>
2022-01-17 16:39:32 -0300

    Aprendizado

--- somaimparesmultiplosdetres.py
+++ somaimparesmultiplosdetres.py
@@ -1,5 +1,17 @@
 # Soma ímpares múltiplos de três.
+soma = input('Informe um valor inicial: ')
+cont = input('Informe um valor final: ')
+for c in range(soma, cont, 2):
+    if c % 3 == 0:
+        cont += 1
+        soma += c
+print("\nA soma de todos os {} valores solicitados é {}.\n".format(cont, soma))
 
+
+
+
+'''
+# Soma ímpares múltiplos de três.
 soma = 0
 cont = 0
 for c in range(1, 501, 2):
@@ -7,3 +19,6 @@ for c in range(1, 501, 2):
         cont += 1
         soma += c
 print("\nA soma de todos os {} valores solicitados é {}.\n".format(cont, soma))
+
+
+'''
