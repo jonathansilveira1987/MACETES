c1c334a0928a145ffd61b497568eddda7d6f049b
Jonathan <jonathan.silveira@outlook.com>
2022-01-19 14:38:44 -0300

    Aprendizado

--- utilidades.py
+++ utilidades.py
@@ -15,7 +15,7 @@ while True:
     Pacote de Ferramentas!
     Escolha abaixo a ferramenta desejada...\n
     [ 01 ] - Apresentação                           [ 21 ] - Verificar se site está acessível
-    [ 02 ] - Imprimir na tela                       [ 22 ] - 
+    [ 02 ] - Imprimir na tela                       [ 22 ] - Sistema interativo de ajuda
     [ 03 ] - Olá Mundo!                             [ 23 ] - 
     [ 04 ] - Teste de conexão de internet           [ 24 ] - 
     [ 05 ] - Loading...                             [ 25 ] - 
@@ -537,17 +537,70 @@ Loading…
         else:
             print(f'\nO site foi acessado com sucesso!')
             # print(site.read())
-
-
-
-
-
-
-    # 
+    # Sistema interativo de ajuda.
     elif opcao == '22':
         print('Disponibilizando ferramenta, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        c = ('\033[m',         # 0 - sem cores
+            '\033[0;30;41m',  # 1 - vermelho
+            '\033[0;30;42m',  # 2 - verde
+            '\033[0;30;43m',  # 3 - amarelo
+            '\033[0;30;44m',  # 4 - azul
+            '\033[0;30;45m',  # 5 - roxo
+            '\033[7;30m'      # 6 - branco
+            )
+        def ajuda(com):
+            titulo(f'Acessando o manual do comando \'{com}\'', 4)
+            print(c[6], end='')
+            help(com)
+            print(c[0], end='')
+            sleep(2)
+        def titulo(msg, cor=0):
+            tam = len(msg) + 4
+            print(c[cor], end='')
+            print('~' * tam)
+            print(f'  {msg}')
+            print('~' * tam)
+            print(c[0], end='')
+            sleep(1)
+        # Programa Principal
+        comando = ''
+        while True:
+            titulo('SISTEMA DE AJUDA PyHELP', 2)
+            comando = str(input("Função ou Biblioteca > "))
+            if comando.upper == 'FIM':
+                break
+            else:
+                ajuda(comando)
+        titulo('ATÉ LOGO!', 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     # 
     elif opcao == '23':
         print('Disponibilizando ferramenta, por favor aguarde...')
