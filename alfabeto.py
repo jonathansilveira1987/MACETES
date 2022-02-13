3f80dc5d815b1370dbb23355b3e57dec1b894751
Jonathan <jonathan.silveira@outlook.com>
2022-02-13 15:29:30 -0300

    Aprendizado

--- alfabeto.py
+++ alfabeto.py
@@ -3,7 +3,7 @@ from time import sleep
 while True:
     # Programa principal!
 
-    letra = str(input("\nInforme a letra do alfabeto que deseja exibir (0 para encerrar) > "))
+    letra = str(input("\nInforme a letra do alfabeto que deseja exibir (0 para encerrar): "))
 
     # Encerrar aplicação.
     if letra in '0':
@@ -12,7 +12,7 @@ while True:
     elif letra in 'Aa':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n')
+        print('\033[0;32m')
         for row in range(7):
             for col in range(5):
                 if (row==0) and (col in {1,2,3}):
@@ -24,11 +24,12 @@ while True:
                 else:
                     print(" ", end=" ")
             print()
+        '\033[m'
     # B
     elif letra in 'Bb':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print()
+        print('\033[0;32m')
         for row in range(7):
             for col in range(5):
                 if (row in {0, 3, 6}) and (col in {0, 1, 2, 3}):
@@ -38,11 +39,12 @@ while True:
                 else:
                     print(" ", end=" ")
             print()
+        '\033[m'
     # C
     elif letra in 'Cc':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print()
+        print('\033[0;32m')
         for row in range(7):
             for col in range(5):
                 if (row in {0, 6}) and (col in {1, 2, 3}):
@@ -54,11 +56,12 @@ while True:
                 else:
                     print(" ", end=" ")
             print()
+        '\033[m'
     # D
     elif letra in 'Dd':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print()
+        print('\033[0;32m')
         for row in range(7):
             for col in range(5):
                 if (row in {0, 6}) and (col in {0, 1, 2, 3}):
@@ -68,154 +71,364 @@ while True:
                 else:
                     print(" ", end=" ")
             print()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        '\033[m'
     # E
     elif letra in 'Ee':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range(7):
+            for col in range(5):
+                if (linhas in {0, 3, 6}) or (col == 0):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # F
     elif letra in 'Ff':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range(7):
+            for col in range(5):
+                if (linhas in {0, 3}):
+                    print("*", end=" ")
+                elif (linhas in {1, 2, 3, 4, 5, 6}) and (col == 0):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # G
     elif letra in 'Gg':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range(7):
+            for col in range(5):
+                if (linhas in {0, 6}) and (col in {1, 2, 3}):
+                    print("*", end=" ")
+                elif (linhas in {1, 4, 5}) and (col in {0, 4}):
+                    print("*", end=" ")
+                elif (linhas == 2) and (col == 0):
+                    print("*", end=" ")
+                elif (linhas == 3) and (col != 1):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # H
     elif letra in 'Hh':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range(7):
+            for col in range(5):
+                if (linhas == 3) or (col in {0, 4}):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # I
     elif letra in 'Ii':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range(7):
+            for col in range(5):
+                if (linhas in {0, 6}) or (col == 2):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # J
     elif letra in 'Jj':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range(7):
+            for col in range(5):
+                if (linhas == 0):
+                    print("*", end=" ")
+                elif (linhas in {1, 2, 3, 4}) and (col == 2):
+                    print("*", end=" ")
+                elif (linhas == 5) and (col in {0, 2}):
+                    print("*", end=" ")
+                elif (linhas == 6) and (col == 1):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # K
     elif letra in 'Kk':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range(7):
+            for col in range(5):
+                if (linhas in {0, 6}) and (col in {0, 4}):
+                    print("*", end=" ")
+                elif (linhas in {1, 5}) and (col in {0, 3}):
+                    print("*", end=" ")
+                elif (linhas in {2, 4}) and (col in {0, 2}):
+                    print("*", end=" ")
+                elif (linhas == 3) and (col in {0, 1}):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # L
     elif letra in 'Ll':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range(7):
+            for col in range(5):
+                if (linhas != 6) and (col == 0):
+                    print("*", end=" ")
+                elif (linhas == 6):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # M
     elif letra in 'Mm':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range(7):
+            for col in range(5):
+                if (linhas not in {1, 2}) and (col in {0, 4}):
+                    print("*", end=" ")
+                elif (linhas == 1) and (col != 2):
+                    print("*", end=" ")
+                elif (linhas == 2) and (col in {0, 2, 4}):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # N
     elif letra in 'Nn':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range(7):
+            for col in range(5):
+                if (linhas in {0, 1, 5, 6}) and (col in {0, 4}):
+                    print("*", end=" ")
+                elif (linhas == 2) and (col in {0, 1, 4}):
+                    print("*", end=" ")
+                elif (linhas == 3) and (col in {0, 2, 4}):
+                    print("*", end=" ")
+                elif (linhas == 4) and (col in {0, 3, 4}):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # O
     elif letra in 'Oo':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range(7):
+            for col in range(5):
+                if (linhas in {0, 6}) and (col in {1, 2, 3}):
+                    print("*", end=" ")
+                elif (linhas in {1, 2, 3, 4, 5}) and (col in {0,4}):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # P
     elif letra in 'Pp':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range(7):
+            for col in range(5):
+                if (linhas in {0, 3}) and (col != 4):
+                    print("*", end=" ")
+                elif (linhas in {1, 2}) and (col in {0,4}):
+                    print("*", end=" ")
+                elif (linhas in {4, 5, 6}) and (col == 0):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # Q
     elif letra in 'Qq':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range(7):
+            for col in range(5):
+                if (linhas == 0) and (col in {1, 2, 3}):
+                    print("*", end=" ")
+                elif (linhas in {1, 2, 3}) and (col in {0,4}):
+                    print("*", end=" ")
+                elif (linhas == 4) and (col in {0, 2, 4}):
+                    print("*", end=" ")
+                elif (linhas == 5) and (col in {0, 3, 4}):
+                    print("*", end=" ")
+                elif (linhas == 6) and (col != 0):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # R
     elif letra in 'Rr':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range(7):
+            for col in range(5):
+                if (linhas in {0,3}) and (col != 4):
+                    print("*", end=" ")
+                elif (linhas in {1, 2, 6}) and (col in {0,4}):
+                    print("*", end=" ")
+                elif (linhas == 4) and (col in {0, 2}):
+                    print("*", end=" ")
+                elif (linhas == 5) and (col in {0, 3}):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # S
     elif letra in 'Ss':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
         print()
-        for row in range(7):
+        print('\033[0;32m')
+        for linhas in range(7):
             for col in range(5):
-                if (row in {0,3,6}) and (col in {1,2,3}):
+                if (linhas in {0,3,6}) and (col in {1,2,3}):
                     print("*", end=" ")
-                elif (row in {1,5}) and (col in {0,4}):
+                elif (linhas in {1,5}) and (col in {0,4}):
                     print("*", end=" ")
-                elif (row == 2) and (col == 0):
+                elif (linhas == 2) and (col == 0):
                     print("*", end=" ")
-                elif (row == 4) and (col == 4):
+                elif (linhas == 4) and (col == 4):
                     print("*", end=" ")
                 else:
                     print(" ", end=" ")
             print()
+        '\033[m'
     # T
     elif letra in 'Tt':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
-    # U
-    elif letra in 'Uu':
-        print('Disponibilizando, por favor aguarde...')
-        sleep(2)
-        print()
-        for row in range(7):
+        print('\033[0;32m')
+        for linhas in range(7):
             for col in range(5):
-                if (row != 6) and (col in {0,4}):
+                if (linhas == 0):
                     print("*", end=" ")
-                elif (row == 6) and (col in {1,2,3}):
+                elif (linhas != 0) and (col == 2):
                     print("*", end= " ")
                 else:
                     print(" ", end=" ")
             print()
+        '\033[m'
+    # U
+    elif letra in 'Uu':
+        print('Disponibilizando, por favor aguarde...')
+        sleep(2)
+        print('\033[0;32m')
+        for linhas in range(7):
+            for col in range(5):
+                if (linhas != 6) and (col in {0,4}):
+                    print("*", end=" ")
+                elif (linhas == 6) and (col in {1,2,3}):
+                    print("*", end= " ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # V
     elif letra in 'Vv':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range (7):
+            for col in range(5):
+                if (linhas in {0, 1, 2, 3, 4}) and (col in {0, 4}):
+                    print("*", end=" ")
+                elif (linhas == 5) and (col in {1, 3}):
+                    print("*", end=" ")
+                elif (linhas == 6) and (col == 2):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # W
     elif letra in 'Ww':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range (7):
+            for col in range(5):
+                if (linhas in {0, 1, 5, 6}) and (col in {0, 4}):
+                    print("*", end=" ")
+                elif (linhas in {2, 4}) and (col in {1, 3}):
+                    print("*", end=" ")
+                elif (linhas == 3) and (col == 2):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # X
     elif letra in 'Xx':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range (7):
+            for col in range(5):
+                if (linhas in {0, 1, 5, 6}) and (col in {0, 4}):
+                    print("*", end=" ")
+                elif (linhas in {2, 4}) and (col in {1, 3}):
+                    print("*", end=" ")
+                elif (linhas == 3) and (col == 2):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # Y
     elif letra in 'Yy':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print('\n\033[0;32mDesculpe, ainda não há algoritmo disponível para essa opção.\033[m')
+        print('\033[0;32m')
+        for linhas in range (7):
+            for col in range(5):
+                if (linhas in {0, 1}) and (col in {0, 4}):
+                    print("*", end=" ")
+                elif (linhas == 2) and (col in {1, 3}):
+                    print("*", end=" ")
+                elif (linhas in {3, 4, 5, 6}) and (col == 2):
+                    print("*", end=" ")
+                else:
+                    print(" ", end=" ")
+            print()
+        '\033[m'
     # Z
     elif letra in 'Zz':
         print('Disponibilizando, por favor aguarde...')
         sleep(2)
-        print()
+        print('\033[0;32m')
         for linhas in range (7):
             for col in range(5):
                 if (linhas in {0, 6}):
@@ -233,6 +446,10 @@ while True:
                 else:
                     print(" ", end=" ")
             print()
+        '\033[m'
+
+
+
 
 
 
