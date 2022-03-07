77d8851510d1856d0dea77d8bd3919698ced8279
Jonathan <jonathan.silveira@outlook.com>
2022-03-06 23:51:35 -0300

    Aprendizado

--- prototipo.py
+++ prototipo.py
@@ -1,26 +1,5 @@
 
-v = sm = nv = 0
-while True:
-    v = int(input("\nDigite um valor (\033[0;32m000\033[m para executar a operação): "))
-    if v == 000:
-        break
-    sm += v
-    nv += 1
-print(f"\nO total de valores digitados foi de {nv}.")
-print(f"A soma entre eles é {sm}.")
 
-
-
-
-v = sm = nv = 0
-while v != 999:
-    v = int(input("\nDigite um valor (\033[0;32m000\033[m para executar a operação): "))
-    if v == 000:
-        break
-    sm += v
-    nv += 1
-print(f"\nO total de valores digitados foi de {nv}.")
-print(f"A soma entre eles é {sm}.")
 
 
 
@@ -152,6 +131,31 @@ print(a * b)
 c = 5 / 5.5
 d = 5 * 5.5
 print(c//d)
+
+
+
+v = sm = nv = 0
+while True:
+    v = int(input("\nDigite um valor (\033[0;32m000\033[m para executar a operação): "))
+    if v == 000:
+        break
+    sm += v
+    nv += 1
+print(f"\nO total de valores digitados foi de {nv}.")
+print(f"A soma entre eles é {sm}.")
+
+
+
+
+v = sm = nv = 0
+while v != 999:
+    v = int(input("\nDigite um valor (\033[0;32m000\033[m para executar a operação): "))
+    if v == 000:
+        break
+    sm += v
+    nv += 1
+print(f"\nO total de valores digitados foi de {nv}.")
+print(f"A soma entre eles é {sm}.")
 
 
 
