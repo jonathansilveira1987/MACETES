765c91d61a594c5c9b2f2e4891f2e286402c54aa
Jonathan <jonathan.silveira@outlook.com>
2022-03-11 22:17:13 -0300

    Aprendizado

--- prototipo.py
+++ prototipo.py
@@ -1,14 +1,49 @@
 
 
+# Valor numérico
+while True:
+    try:
+        n = int(input('\ndigite um número de 3 dígitos: '))
+        if 100 <= n <= 999: # é um número de 3 dígitos
+            break # sai do loop
+        print('\nO número deve ter 3 dígitos')
+    except ValueError:
+        print('\nNão foi digitado um número')
+
+
+
+while True:
+    try:
+        texto = input('\ndigite um número de 3 dígitos: ')
+        if texto.isnumeric() and len(texto) == 3: # é um "número" de 3 dígitos
+            break # sai do loop
+        print('\nO número deve ter 3 dígitos')
+    except ValueError:
+        print('\nNão foi digitado um número')
+print(texto[::-1]) # imprime invertido
 
 
 
 
+# invertendo
+inverso = 0
+while n > 0:
+    inverso = inverso * 10 + n % 10
+    n //= 10
+    print(inverso)
 
+#Número negativo
+x = abs(n)
+inverso = 0
+while x > 0:
+    inverso = inverso * 10 + x % 10
+    x //= 10
+    if n < 0:
+        inverso *= -1
 
-
-
-
+# isnumeric
+print('½'.isnumeric()) # True
+print(int('½')) # ValueError
 
 
 
@@ -124,6 +159,23 @@
 
 
 '''
+
+
+num = float(input('\nNumero original: '))
+if num == round(num):
+    print("\nO valor informado é INTEIRO.")
+else:
+    print("\nO valor informado é DECIMAL.")
+    print("\nArredondado pra baixo: ", round(num-0.5))
+    print("Arredondado pra cima : ", round(num+0.5))
+
+
+
+
+
+
+
+
 
 
 a = 100/5
