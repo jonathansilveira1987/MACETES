6adc2d363c1ad1f6572ff108fb83c2eab67d44a4
Jonathan <jonathan.silveira@outlook.com>
2022-03-13 18:55:35 -0300

    Aprendizado

--- prototipo.py
+++ prototipo.py
@@ -1,83 +1,73 @@
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+nome = str(input('\nNome: '))
+while (len(nome) <= 3):
+    print('\n\033[0;31mO nome digitado deve conter mais de 3 caracteres!\033[m')
+    nome = str(input('Nome: '))
+idade = int(input('\nIdade: '))
+while ((idade < 0) or (idade > 150)):
+    print('\n\033[0;31mA idade informada deve ser entre 0 e 150 anos!\033[m')
+    idade = int(input('Idade: '))
+salario = float(input('\nSalário: '))
+while (salario <= 0):
+    print('\n\033[0;31mO salário deve ser superior a 0!\033[m')
+    salario = float(input('Salário: '))
+sexo = input('\nSexo [M - Masculino / F - Feminino]: ')
+while (sexo != 'M') and (sexo != 'F'):
+    print('\n\033[0;31mInformação inválida, tente novamente!\033[m')
+    sexo = str(input('Sexo [M - Masculino / F - Feminino]: '))
+ecivil = input('\nEstado Civil (S, C, V, D): ')
+while (ecivil != 'S') and (ecivil != 'C') and (ecivil != 'V') and (ecivil != 'D'):
+    print('\n\033[0;31mInformação inválida, tente novamente!\033[m')
+    ecivil = str(input('\nEstado Civil (S, C, V, D): '))
+print(f'\nO nome informado foi \033[0;32m{nome}\033[m.')
+print(f'A idade informada foi \033[0;32m{idade}\033[m.')
+print(f'O salário informado foi R$ \033[0;32m{salario:.2f}\033[m.')
+print(f'O sexo informado foi \033[0;32m{sexo}\033[m.')
+print(f'O estado civil informado foi \033[0;32m{ecivil}\033[m.')
+
+
+nome = input("Qual seu nome [mínimo 4 caracteres]: ")
+idade = int(input("Sua idade: "))
+salario = float(input("Salário: "))
+sexo = input("Sexo ('f' para feminino ou 'm' para masculino): ")
+civil = input("Estado civil (s, c, v ou d): ")
+while len(nome) <= 3:
+    nome = input("Seu nome deve ter mais que 3 caracteres: ")
+while (idade < 0) or (idade > 150):
+    idade = int(input("Voce deve ter entre 0 e 150 anos: "))
+while (salario < 0):
+    salario = float(input("A coisa ta difícil, mas não tem salário negativo: "))
+while (sexo!= 'f') and (sexo!='m'):
+    sexo = input("Biologicamente, você deve ser 'f' ou 'm': ")
+while (civil != 's') and (civil != 'c') and (civil != 'v') and (civil != 'd'):
+    print("Nao tem estado civil 'confuso'")
+    civil = input("Deve ser s, c, v ou d: ")
+
+
+while True:
+    usuario = input('\nUsuário: ')
+    senha = input('Senha: ')
+    if usuario == senha:
+        print("\n\033[0;31mO usuário e a senha não podem ser iguais, tente novamente!\033[m")
+    else:
+        print(f'\nO usuário informado foi {usuario}.')
+        print(f'A senha informada foi {senha}.')
+        break
+while True:
+    nota = int(input('\nInforme uma nota entre 0 e 10: '))
+    if nota > 10:
+        print(f'\n{nota} é um valor inválido, tente novamente!')
+    else:
+        print(f'\nVocê digitou a nota {nota}.')
+        break
+
+login = input("Login: ")
+senha = input("Senha: ")
+while login == senha:
+    print("Sua senha deve ser diferente do login: ")
+    senha = input("Senha: ")
+print("Cadastro aprovado")
 
 
 
@@ -118,6 +108,8 @@
 
 
 '''
+
+
 
 
 num = float(input('\nNumero original: '))
