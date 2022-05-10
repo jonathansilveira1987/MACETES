f551ab5b8bdff97008ba91b798ed5987475488de
Jonathan <jonathan.silveira@outlook.com>
2022-05-09 21:47:42 -0300

    Aprendizado

---
+++ proto.py
@@ -0,0 +1,5 @@
+# Calcular Percentual.
+valor = float(input('\nValor: '))
+percentual = float(input('Percentual: '))
+resultado = valor * percentual / 100
+print(f'\n{percentual}% de {valor} é {resultado:.2f}.')
