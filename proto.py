da82d2f229934df15cb35e8d47ee8cc88df9e8cc
Jonathan <jonathan.silveira@outlook.com>
2022-05-22 20:34:51 -0300

    Aprendizado

---
+++ proto.py
@@ -0,0 +1,7 @@
+# Calcular Percentual
+a = float(input('\nVetor A: '))
+b = float(input('Vetor B: '))
+c = b * 100
+result = c / a
+print(f'\nO percentual de \033[0;32m{a}\033[m - \033[0;32m{b}\033[m é \033[0;33m{result:.2f}%\033[m')
+print(f'\nO percentual obtido é de \033[0;33m{result:.2f}%\033[m')
--- prototipo.py
+++ prototipo.py
@@ -1,4 +1,12 @@
-# Calcular Percentual.
+# Calcular Percentual
+a = float(input('\nVetor A: '))
+b = float(input('Vetor B: '))
+c = b * 100
+result = c / a
+print(f'\nO percentual de \033[0;32m{a}\033[m - \033[0;32m{b}\033[m é \033[0;33m{result:.2f}%\033[m')
+print(f'\nO percentual obtido é de \033[0;33m{result:.2f}%\033[m')
+
+# Calcular Percentual Dinheiro.
 valor = float(input('\nValor R$ '))
 percentual = float(input('Percentual desejado % '))
 resultado = valor * percentual / 100
