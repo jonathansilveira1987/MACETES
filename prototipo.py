8a890eb805757b3c438f75ce000566b94514798d
Jonathan <jonathan.silveira@outlook.com>
2022-05-22 19:57:27 -0300

    Aprendizado

--- prototipo.py
+++ prototipo.py
@@ -1,4 +1,13 @@
+# Calcular Percentual.
+valor = float(input('\nValor R$ '))
+percentual = float(input('Percentual desejado % '))
+resultado = valor * percentual / 100
+print(f'\n\033[0;31m{percentual}%\033[m de \033[0;32m{valor:.2f}\033[m é \033[0;33m{resultado:.2f}\033[m')
 
+valor_real = "\033[0;31m{}%\033[m de \033[0;32mR$ {:,.2f}\033[m é \033[0;33mR$ {:,.2f}\033[m".format(percentual, valor, resultado).replace(",", "X").replace(".", ",").replace("X", ".")
+print()
+print(valor_real)
+print()
 
 # Faça um Programa que leia uma lista de 5 números inteiros e mostre-os.
 print()
