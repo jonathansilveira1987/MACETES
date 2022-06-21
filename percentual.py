8532ca8e826f370b98c2cab712ec8fee6c256417
Jonathan <jonathan.silveira@outlook.com>
2022-06-20 23:05:46 -0300

    Aprendizado

--- percentual.py
+++ percentual.py
@@ -1,10 +1,36 @@
+from time import sleep
+
+byte = 8
+
+# Modo Manual.
+# 9. YottaByte
+
+yottabyte = 1 * 1024
+byte = 2 ** 80
+bit = (2 ** 80) * 8
+print('Calculando...')
+sleep(3)
+print("\033[0;32m\n-> 1 YottaByte possui {0:,} ZettaBytes.\033[m".format(yottabyte).replace(",", "."))
+sleep(3)
+print("\033[0;33m-> 1 YottaByte possui {0:,} Bytes.\033[m".format(byte).replace(",", "."))
+sleep(3)
+print("\033[0;34m-> 1 YottaByte possui {0:,} Bits.\033[m\n".format(bit).replace(",", "."))
+sleep(3)
+print("Abaixo segue número lido por extenso...")
+sleep(3)
+print("\033[0;31m9 septilhões .671 sextilhões .406 quintilhões .556 quatrilhões .917 trilhões .033 bilhões .397 milhôes .649 mil .408 bits.\033[m\n")
 
 
-# Calcular Percentual
-a = float(input('\nVetor A: '))
-b = float(input('Vetor B: '))
-c = b * 100
-result = c / a
-print(f'\nO percentual de \033[0;32m{a}\033[m - \033[0;32m{b}\033[m é \033[0;33m{result:.2f}%\033[m')
-print(f'\nO percentual obtido é de \033[0;33m{result:.2f}%\033[m')
 
+# Modo Automático.
+# 9. YottaByte
+
+print('Calculando...')
+b1 = 2 ** 80
+b1r = '{0:,}'.format(b1).replace(',','.') # Aqui coloca os pontos
+yottabyte = byte * b1
+b2r = '{0:,}'.format(yottabyte).replace(',','.') # Aqui coloca os pontos
+sleep(2)
+print(f'\n\033[0;33m1 YottaByte equivale a {b1r} Bytes.')
+sleep(2)
+print(f'\033[0;32m1 YottaByte equivale a {b2r} Bits.\n')
