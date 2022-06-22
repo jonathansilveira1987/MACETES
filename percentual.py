ba0e65500bb1e3cb1d646b1ac01f5077820541ef
Jonathan <jonathan.silveira@outlook.com>
2022-06-21 23:55:30 -0300

    Aprendizado

--- percentual.py
+++ percentual.py
@@ -3,22 +3,38 @@ from time import sleep
 byte = 8
 
 # Modo Manual.
-# 9. YottaByte
-
-yottabyte = 1 * 1024
-byte = 2 ** 80
-bit = (2 ** 80) * 8
+# 10. Brontobyte.
+brontobyte = 1 * 1024
+byte = 10 ** 27
+bit = (10 ** 27) * 8
 print('Calculando...')
 sleep(3)
-print("\033[0;32m\n-> 1 YottaByte possui {0:,} ZettaBytes.\033[m".format(yottabyte).replace(",", "."))
+print("\033[0;32m\n-> 1 Brontobyte possui {0:,} YottaBytes.\033[m".format(brontobyte).replace(",", "."))
 sleep(3)
-print("\033[0;33m-> 1 YottaByte possui {0:,} Bytes.\033[m".format(byte).replace(",", "."))
+print("\033[0;33m-> 1 Brontobyte possui {0:,} Bytes.\033[m".format(byte).replace(",", "."))
 sleep(3)
-print("\033[0;34m-> 1 YottaByte possui {0:,} Bits.\033[m\n".format(bit).replace(",", "."))
+print("\033[0;34m-> 1 Brontobyte possui {0:,} Bits.\033[m\n".format(bit).replace(",", "."))
 sleep(3)
 print("Abaixo segue número lido por extenso...")
 sleep(3)
-print("\033[0;31m9 septilhões .671 sextilhões .406 quintilhões .556 quatrilhões .917 trilhões .033 bilhões .397 milhôes .649 mil .408 bits.\033[m\n")
+print("\033[0;31m8 octilhões de bits.\033[m\n")
+# 11. Geopbyte.
+geopbyte = 1 * 1024
+byte = 10 ** 30
+bit = (10 ** 30) * 8
+print('Calculando...')
+sleep(3)
+print("\033[0;32m\n-> 1 Geopbyte possui {0:,} Brontobytes.\033[m".format(geopbyte).replace(",", "."))
+sleep(3)
+print("\033[0;33m-> 1 Geopbyte possui {0:,} Bytes.\033[m".format(byte).replace(",", "."))
+sleep(3)
+print("\033[0;34m-> 1 Geopbyte possui {0:,} Bits.\033[m\n".format(bit).replace(",", "."))
+sleep(3)
+print("Abaixo segue número lido por extenso...")
+sleep(3)
+print("\033[0;31m8 nonilhões de bits.\033[m\n")
+
+
 
 
 
@@ -34,3 +50,32 @@ sleep(2)
 print(f'\n\033[0;33m1 YottaByte equivale a {b1r} Bytes.')
 sleep(2)
 print(f'\033[0;32m1 YottaByte equivale a {b2r} Bits.\n')
+
+
+
+
+print('''
+
+
+escala	escala		notação	prefixo	símbolo
+curta	longa		científica	SI*	SI*
+um	um	1	100		
+mil	mil	1000	103	quilo	k
+milhão	milhão	1 000 000	106	mega	M
+bilhão	mil milhões	1 000 000 000	109	giga	G
+trilião	bilião	1 000 000 000 000	1012	tera	T
+quatrilhão	mil biliões	1 000 000 000 000 000	1015	peta	p
+quintilhão	trilião	1 000 000 000 000 000 000	1018	exa	E
+sextilhão	mil triliões	1 000 000 000 000 000 000 000	1021	zetta	Z
+setilhão	quatrilião	1 000 000 000 000 000 000 000 000	1024	yota	Y
+octilhão	mil quatriliões	1 000 000 000 000 000 000 000 000 000	1027		
+nonilhão	quintilião	1 000 000 000 000 000 000 000 000 000 000	1030		
+decilhão	mil quintiliões	1 000 000 000 000 000 000 000 000 000 000 000	1033		
+undecilhão	sextilião	1 000 000 000 000 000 000 000 000 000 000 000 000	1036		
+dodecilhão	mil sextiliões	1 000 000 000 000 000 000 000 000 000 000 000 000 000	1039		
+tredecilhão	septilião	1 000 000 000 000 000 000 000 000 000 000 000 000 000 000	1042		
+
+
+
+
+''')
