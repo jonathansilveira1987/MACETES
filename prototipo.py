cafcf699bbdd83dea494d2885f4d9ee187905d54
Jonathan <jonathan.silveira@outlook.com>
2022-07-04 00:04:01 -0300

    Aprendizado

--- prototipo.py
+++ prototipo.py
@@ -3,40 +3,12 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-print('''
-
-
-escala	escala		notação	prefixo	símbolo
-curta	longa		científica	SI*	SI*
-um	um	1	100		
-mil	mil	1000	103	quilo	k
-milhão	milhão	1 000 000	106	mega	M
-bilhão	mil milhões	1 000 000 000	109	giga	G
-trilião	bilião	1 000 000 000 000	1012	tera	T
-quatrilhão	mil biliões	1 000 000 000 000 000	1015	peta	p
-quintilhão	trilião	1 000 000 000 000 000 000	1018	exa	E
-sextilhão	mil triliões	1 000 000 000 000 000 000 000	1021	zetta	Z
-setilhão	quatrilião	1 000 000 000 000 000 000 000 000	1024	yota	Y
-octilhão	mil quatriliões	1 000 000 000 000 000 000 000 000 000	1027		
-nonilhão	quintilião	1 000 000 000 000 000 000 000 000 000 000	1030		
-decilhão	mil quintiliões	1 000 000 000 000 000 000 000 000 000 000 000	1033		
-undecilhão	sextilião	1 000 000 000 000 000 000 000 000 000 000 000 000	1036		
-dodecilhão	mil sextiliões	1 000 000 000 000 000 000 000 000 000 000 000 000 000	1039		
-tredecilhão	septilião	1 000 000 000 000 000 000 000 000 000 000 000 000 000 000	1042		
-''')
-
+import calendar
+yy = int(input('\nAno: '))
+mm = int(input('Mês: '))
+print('\033[0;33m')
+print(calendar.month(yy, mm))
+print('\033[m')
 
 
 
