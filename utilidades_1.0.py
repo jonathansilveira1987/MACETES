4104f97a53f64f1ed865fd7693434bb6ced5132a
jonathansilveira1987 <jonathan.silveira@outlook.com>
2022-07-08 21:29:56 -0300

    Aprendizado

--- utilidades_1.0.py
+++ utilidades_1.0.py
@@ -349,10 +349,10 @@ Loading…
             # Aqui vai o programa principal!
             texto = str(input('\nDigite seu texto: '))
             # "Aprendendo Python na disciplina de linguagem de programação."
-            print(f"\nTamanho do texto = {len(texto)}")
+            print(f"\nO texto informado possui {len(texto)} caracteres.")
             print(f"\nPython in texto = {'Python' in texto}")
             print(f"\nQuantidade de y no texto = {texto.count('y')}")
-            print(f"\nAs 3 primeiras letras são: {texto[0:3]}\n")
+            print(f"\nAs 3 primeiras letras são: {texto[0:3]}")
             # Aqui vai o "Deseja continuar?"
             resp = " "
             while resp not in "10":
