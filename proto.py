3a16a887b87383c27bdc4d5fafcbe25f9001d551
jonathansilveira1987 <jonathan.silveira@outlook.com>
2022-07-08 23:41:35 -0300

    Aprendizado

--- proto.py
+++ proto.py
@@ -1,5 +1,4 @@
-texto = str(input('\nDigite seu texto: '))
-# Aprendendo Python na disciplina de linguagem de programação.
+texto = str(input('\nDigite seu texto: ')) # Aprendendo Python na disciplina de linguagem de programação.
 print(f"\nO texto informado possui \033[0;31m{len(texto)}\033[m caracteres.")
 print(f"\nPython in texto = \033[0;31m{'Python' in texto}\033[m")
 print(f"\nQuantidade de y no texto = \033[0;31m{texto.count('y')}\033[m")
