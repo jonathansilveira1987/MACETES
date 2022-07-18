4163b9be38e300154a03ee2c9537faea197161c0
jonathansilveira1987 <jonathan.silveira@outlook.com>
2022-07-17 23:48:34 -0300

    Aprendizado

--- proto.py
+++ proto.py
@@ -1,11 +1,17 @@
 import platform
+import wmi # Usando o módulo WMI (apenas para Windows) pode ser instalado usando o comando: pip install wmi
+c = wmi.WMI()    
+my_system = c.Win32_ComputerSystem()[0]
 
-print(f'\nSistema Operacional: \033[0;31m{platform.system()}\033[m')
-print(f'Versão: \033[0;31m{platform.release()}\033[m')
+print(f'\nNome do Computador: \033[0;31m{platform.node()}\033[m')
+print(f'Sistema Operacional: \033[0;31m{platform.system()}\033[m')
+print(f'Lançamento: \033[0;31m{platform.release()}\033[m')
+print(f'Versão: \033[0;31m{platform.version()}\033[m')
+print(f'Informações da Distribuição: \033[0;31m{platform.platform()}\033[m')
+print(f"Tipo de Sistema: \033[0;31m{my_system.SystemType}\033[m")
 print(f'Arquitetura do Processador: \033[0;31m{platform.machine()}\033[m')
-print(f'Nome do Computador: \033[0;31m{platform.node()}\033[m')
-print(f'Informações da Distribuição: \033[0;31m{platform.platform()}\033[m\n')
-
-
-
-
+print(f'Processador: \033[0;31m{platform.processor()}\033[m')
+print(f"Número de Processadores: \033[0;31m{my_system.NumberOfProcessors}\033[m")
+print(f"Fabricante do Equipamento: \033[0;31m{my_system.Manufacturer}\033[m")
+print(f"Modelo do Equipamento: \033[0;31m{my_system. Model}\033[m")
+print(f"Família do Equipamento: \033[0;31m{my_system.SystemFamily}\033[m\n")
