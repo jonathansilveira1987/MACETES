f311693b9b7044dff0de3e3fd3da8fcadfd79e72
jonathansilveira1987 <jonathan.silveira@outlook.com>
2022-07-17 21:13:18 -0300

    Aprendizado

--- proto.py
+++ proto.py
@@ -1,5 +1,11 @@
-texto = str(input('\nDigite seu texto: ')) # Aprendendo Python na disciplina de linguagem de programação.
-print(f"\nO texto informado possui \033[0;31m{len(texto)}\033[m caracteres.")
-print(f"\nPython in texto = \033[0;31m{'Python' in texto}\033[m")
-print(f"\nQuantidade de y no texto = \033[0;31m{texto.count('y')}\033[m")
-print(f"\nAs 3 primeiras letras são: \033[0;31m{texto[0:3]}\033[m\n")
+import platform
+
+print(f'\nSistema Operacional: \033[0;31m{platform.system()}\033[m')
+print(f'Versão: \033[0;31m{platform.release()}\033[m')
+print(f'Arquitetura do Processador: \033[0;31m{platform.machine()}\033[m')
+print(f'Nome do Computador: \033[0;31m{platform.node()}\033[m')
+print(f'Informações da Distribuição: \033[0;31m{platform.platform()}\033[m\n')
+
+
+
+
--- prototipo.py
+++ prototipo.py
@@ -1,6 +1,12 @@
 
 
 
+texto = str(input('\nDigite seu texto: ')) # Aprendendo Python na disciplina de linguagem de programação.
+print(f"\nO texto informado possui \033[0;31m{len(texto)}\033[m caracteres.")
+print(f"\nPython in texto = \033[0;31m{'Python' in texto}\033[m")
+print(f"\nQuantidade de y no texto = \033[0;31m{texto.count('y')}\033[m")
+print(f"\nAs 3 primeiras letras são: \033[0;31m{texto[0:3]}\033[m\n")
+
 
 
 import calendar
