e985b83077414b883754af58368428843d8b294f
jonathansilveira1987 <jonathan.silveira@outlook.com>
2022-07-31 20:45:30 -0300

    Aprendizado

--- bem_vindo.py
+++ bem_vindo.py
@@ -1,10 +1,10 @@
-# from operator import index
-
+from operator import index
 from ast import main
-
+from time import sleep
 
 print('\nOlá Mundo!')
 
+sleep(1.5)
 a = type('meu_email@email.com')
 b = type(15)
 c = type(0.26)
@@ -16,9 +16,11 @@ print(f'\033[0;33m{c}\033[m')
 print(f'\033[0;34m{d}\033[m')
 print(f'\033[0;35m{e}\033[m')
 
+sleep(1.5)
 f = '\033[0;36mMeu livro favroito chama-se "Código Limpo".\033[m'
 print(f'\n{f}')
 
+sleep(1.5)
 fruta = " Banana\033[m"
 tipo  = "\n\033[0;30mBolo de"
 print(tipo + fruta)
@@ -26,18 +28,22 @@ print(tipo + fruta)
 # nome = "\033[0;32m0 1 0 1 \033[m"
 # print(nome * 1000)
 
+sleep(1.5)
 g = 'meu_email@email.com\n'
 print(g[::-1])
 
+sleep(1.5)
 h = 5
 print(f'\n{h} multiplicado por 2 é: {h * 2}')
 
+sleep(1.5)
 i = 'JONATHAN DA COSTA SILVEIRA'
 j = [10, 20, 30, 40]
 print(f'\n{i.upper()}') # Todas letras maiúsculas.
 print(f'{i.lower()}') # Todas letras minúsculas.
 print(f'{i.capitalize()}') # Apenas primeira letra minúscula.
 
+sleep(1.5)
 k = [1, 2, 3, 4, 5, 6, 7, 8]
 print(f'\n{k} lista com {len(k)} ítens.')
 l = ['a', 'b', 'c', 'd']
@@ -45,24 +51,20 @@ print(f'\n{l} lista com {len(l)} ítens.')
 m = [3.4, 2.4, 2.6, 6.5, 9.0, 1.5, 8.3]
 print(f'\n{m} lista com {len(m)} ítens.')
 
+sleep(1.5)
 n = [1, 2, 3, 4]
 o = input('\nInformação: ')
 n.append(o)
 print(f'\n{n}')
 
+sleep(1.5)
 print(f'\n{k[5]}')
 print(f'{k[0:3]}')
 print(f'{k[::6]}')
 print(f'{k[::-2]}')
 print(f'{k[7:1:-1]}')
 
-
-
-
-
-
-
-
+sleep(1.5)
 
 
 
--- prototipo.py
+++ prototipo.py
@@ -1,6 +1,41 @@
 
 
 
+
+
+
+
+
+
+
+import platform
+import psutil # pip install psutil
+import wmi # Usando o módulo WMI (apenas para Windows) pode ser instalado usando o comando: pip install wmi
+c = wmi.WMI()    
+my_system = c.Win32_ComputerSystem()[0]
+
+print(f'\nNome do Computador: \033[0;31m{platform.node()}\033[m')
+print(f'Sistema Operacional: \033[0;31m{platform.system()}\033[m')
+print(f'Lançamento: \033[0;31m{platform.release()}\033[m')
+print(f'Versão: \033[0;31m{platform.version()}\033[m')
+print(f'Informações da Distribuição: \033[0;31m{platform.platform()}\033[m')
+print(f"Tipo de Sistema: \033[0;31m{my_system.SystemType}\033[m")
+print(f'Arquitetura do Processador: \033[0;31m{platform.machine()}\033[m')
+print(f'Processador: \033[0;31m{platform.processor()}\033[m')
+print(f"Quantidade de Núcleos do Processador: \033[0;31m{psutil.cpu_count()}\033[m")
+print(f"Número de Processadores: \033[0;31m{my_system.NumberOfProcessors}\033[m")
+print(f"Memória: \033[0;31m{psutil.virtual_memory()}\033[m")
+print(f"Fabricante do Equipamento: \033[0;31m{my_system.Manufacturer}\033[m")
+print(f"Modelo do Equipamento / Placa Mãe: \033[0;31m{my_system. Model}\033[m")
+print(f"Família do Equipamento: \033[0;31m{my_system.SystemFamily}\033[m\n")
+
+f = open("systeminfo.txt","w");
+f.write(str(my_system))
+
+
+
+
+
 import wmi # Usando o módulo WMI (apenas para Windows) pode ser instalado usando o comando: pip install wmi
 c = wmi.WMI()    
 my_system = c.Win32_ComputerSystem()[0]
