1757ea9dd5ea65d2747e813ff57fdedd4ad5f20d
jonathansilveira1987 <jonathan.silveira@outlook.com>
2022-08-17 00:06:09 -0300

    Aprendizado

--- calculadora_em_3_linhas.py
+++ calculadora_em_3_linhas.py
@@ -1,3 +1,16 @@
-operacao_matematica = input('\nDigite uma operação matemática > ')
-resultado = eval(operacao_matematica)
-print(f'\nO resultado é \033[0;31m{resultado}\033[m.\n')
+while True:
+    # Programa principal!
+
+    operacao_matematica = input('\nDigite uma operação matemática: ')
+    resultado = eval(operacao_matematica)
+    print(f'\nO resultado é \033[0;31m{resultado}\033[m.\n')
+    result = 'O resultado é \033[0;31m{0:,}\033[m.\n'.format(resultado).replace(',','.') #Aqui coloca os pontos
+    print(f'{result}')
+
+    # Aqui vai o "Deseja continuar?"
+    resp = " "
+    while resp not in "10":
+        resp = str(input("\033[0;34mDeseja continuar [1 - SIM / 0 - NÃO]? \033[m")).strip().upper()[0]
+    if resp == "0":
+        break    
+print("\033[0;36;1;4m\nVocê optou por finalizar!\033[m\n")
