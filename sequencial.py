ab232eb849aea386ffae2fdf41065b84fec9499f
jonathansilveira1987 <jonathan.silveira@outlook.com>
2022-08-21 20:51:57 -0300

    Aprendizado

--- sequencial.py
+++ sequencial.py
@@ -1,8 +1,8 @@
 lista = [[]]
 n = 0
 quantidade = int(input('\nInforme a quantidade de números da lista: '))
+print()
 for c in range(1, quantidade+1):
-    n = int(input(f"Digite um valor {c}º: "))
+    n = int(input(f'{c}º valor: '))
     lista[0].append(n)
 print(f'\n\033[0;32m{lista[0]}\033[m\n')
-
