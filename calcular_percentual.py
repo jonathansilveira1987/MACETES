1116ba6165b7a9bf887936301c6c2a9d0c0353e8
jonathansilveira1987 <jonathan.silveira@outlook.com>
2022-08-27 17:25:30 -0300

    Aprendizado

--- calcular_percentual.py
+++ calcular_percentual.py
@@ -5,7 +5,7 @@ while True:
     c = (b * a / 100)
     valor_real1 = '\033[0;33mR$ {:,.2f}\033[m'.format(a).replace(",", "X").replace(".", ",").replace("X", ".")
     valor_real2 = '\033[0;32mR$ {:,.2f}\033[m'.format(c).replace(",", "X").replace(".", ",").replace("X", ".")
-    print(f'\n\033[0;31m{b:.1f}%\033[m de {valor_real1} = {valor_real2}.')
+    print(f'\n\033[0;31m{b:.2f}%\033[m de {valor_real1} = {valor_real2}.')
     d = a - c
     valor_real3 = '\033[0;34mR$ {:,.2f}\033[m'.format(d).replace(",", "X").replace(".", ",").replace("X", ".")
     print(f'\nSaldo = {valor_real3}.\n')
--- calculo_fgts.py
+++ calculo_fgts.py
@@ -1,95 +1,24 @@
-
-
-
-
-a = float(input('\nValor A: '))
-b = float(input('Valor B: '))
-
-c = a + b
-d = a - b
-e = (b * a / 100)
-g = ((b - a) / 100) * -1
-i = ((b) / 100)
-
-print(f'\n{c}.')
-print(f'\n{d}.')
-print(f'\n{e}.')
-print(f'\n{g}.')
-print(f'\n{i}.\n')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-
-print (abs(-45))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-a = float(input('\bValor guardado: '))
+mp = float(input('\nValor guardado Mercado Pago: '))
+pp = float(input('Valor guardado PicPay: '))
+nb = float(input('Valor guardado NuBank: '))
+result = mp + pp + nb
+a = result
 b = float(input('Valor FGTS depositado em outubro: '))
 c = float(input('Valor CNH: '))
 d = float(input('Valor FGTS atual: '))
 
 e = a - c
-print(f'\nValor guardado - valor CNH = {e}.')
+valor_real1 = '\033[0;31mR$ {:,.2f}\033[m'.format(e).replace(",", "X").replace(".", ",").replace("X", ".")
+print(f'\nValor guardado - valor CNH = {valor_real1}.')
+
 f = e + b
-print(f'\nValor guardado + FGTS depositado em outubro = {f}.')
+valor_real2 = '\033[0;32mR$ {:,.2f}\033[m'.format(f).replace(",", "X").replace(".", ",").replace("X", ".")
+print(f'\n(Saldo Valor guardado - valor CNH) + FGTS depositado em outubro = {valor_real2}.')
+
 g = d - b
-print(f'\nValor FGTS atual - FGTS depositado em outubro = {g}.')
-'''
+valor_real3 = '\033[0;33mR$ {:,.2f}\033[m'.format(g).replace(",", "X").replace(".", ",").replace("X", ".")
+print(f'\nFGTS atual - FGTS depositado em outubro = {valor_real3}.')
+
+h = f + g
+valor_real4 = '\033[0;34mR$ {:,.2f}\033[m'.format(h).replace(",", "X").replace(".", ",").replace("X", ".")
+print(f'\n(FGTS atual - FGTS depositado em outubro) + Valor guardado atualizado = {valor_real4}.\n')
