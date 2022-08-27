e13fa88a4ca38dc90f89d9a166326ec2cadc9c48
jonathansilveira1987 <jonathan.silveira@outlook.com>
2022-08-27 15:24:08 -0300

    Aprendizado

---
+++ calculo_fgts.py
@@ -0,0 +1,95 @@
+
+
+
+
+a = float(input('\nValor A: '))
+b = float(input('Valor B: '))
+
+c = a + b
+d = a - b
+e = (b * a / 100)
+g = ((b - a) / 100) * -1
+i = ((b) / 100)
+
+print(f'\n{c}.')
+print(f'\n{d}.')
+print(f'\n{e}.')
+print(f'\n{g}.')
+print(f'\n{i}.\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+print (abs(-45))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a = float(input('\bValor guardado: '))
+b = float(input('Valor FGTS depositado em outubro: '))
+c = float(input('Valor CNH: '))
+d = float(input('Valor FGTS atual: '))
+
+e = a - c
+print(f'\nValor guardado - valor CNH = {e}.')
+f = e + b
+print(f'\nValor guardado + FGTS depositado em outubro = {f}.')
+g = d - b
+print(f'\nValor FGTS atual - FGTS depositado em outubro = {g}.')
+'''
