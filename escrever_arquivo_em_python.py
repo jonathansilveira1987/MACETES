03d8fd6c7c33948d855863cbfa2b8aa56abbf95b
jonathansilveira1987 <jonathan.silveira@outlook.com>
2022-08-28 15:38:16 -0300

    Aprendizado

--- escrever_arquivo_em_python.py
+++ escrever_arquivo_em_python.py
@@ -1,8 +1,16 @@
+# Escrever arquivo em Python.
 elemento = int(input('\nInforme o número de elementos para o arquivo: '))
 print()
 palavras = [input("Dado: ") for i in range(elemento)]
 print(f'\n\033[0;32m{palavras}\033[m')
 print('\n\033[0;36mOs dados informados foram salvos no arquivo => a1!\033[m\n')
-with open('a1.txt', 'a') as arquivo:
+with open('data_base.txt', 'a') as arquivo:
     for palavras in palavras:
         arquivo.write(palavras + '\n')
+
+# Excluir arquivo em Python.
+import os
+if os.path.exists('data_base.txt'):
+    os.remove('data_base.txt')
+else:
+    print('\nO arquivo informado não existe!\n')
