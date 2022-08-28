349bbeab7bfdc0c74e354f67163da91aeb652283
jonathansilveira1987 <jonathan.silveira@outlook.com>
2022-08-28 10:52:50 -0300

    Aprendizado

--- calculo_fgts.py
+++ calculo_fgts.py
@@ -21,4 +21,16 @@ print(f'\nFGTS atual - FGTS depositado em outubro = {valor_real3}.')
 
 h = f + g
 valor_real4 = '\033[0;34mR$ {:,.2f}\033[m'.format(h).replace(",", "X").replace(".", ",").replace("X", ".")
-print(f'\n(FGTS atual - FGTS depositado em outubro) + Valor guardado atualizado = {valor_real4}.\n')
+print(f'\n(FGTS atual - FGTS depositado em outubro) + Valor guardado atualizado = {valor_real4}.')
+
+i = (28 * g / 100)
+j = h - i
+valor_real5 = '\033[0;35mR$ {:,.2f}\033[m'.format(j).replace(",", "X").replace(".", ",").replace("X", ".")
+print(f'\nValor Final - Juros empréstimo FGTS = {valor_real5}.')
+
+moto = float(input('\nValor da moto: '))
+cnh = float(input('Valor da CNH: '))
+
+k = j - moto - cnh
+valor_real6 = '\033[0;36mR$ {:,.2f}\033[m'.format(k).replace(",", "X").replace(".", ",").replace("X", ".")
+print(f'\nApós comprar a moto e retirar CNH restará um saldo de = {valor_real6}.\n')
