a8ffec30d34854653da8a336e9dc60b6c3e93ac4
jonathansilveira1987 <jonathan.silveira@outlook.com>
2022-09-02 20:38:23 -0300

    Aprendizado

--- calcular_volume.py
+++ calcular_volume.py
@@ -4,7 +4,7 @@ pi = 3.14159
 cont = 20  # o raio precisa variar 20 vezes
 while cont > 0:
     volume = 4 * pi * (r * r * r) / 3
-    print("O volume com o Raio %4f" % r, "|| volume = %4f" % volume)
+    print("\033[0;34mO volume com o Raio\033[m %4f" % r, "\033[0;32m||\033[m \033[0;34mvolume =\033[m %4f" % volume)
     r = r + 0.5 # aqui faz a variação
     cont = cont - 1 # decrementa o contador
 
