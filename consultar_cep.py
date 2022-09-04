bada083fdef2e9ea1e1a3caefec672da3c6cb184
jonathansilveira1987 <jonathan.silveira@outlook.com>
2022-09-04 16:06:23 -0300

    Aprendizado

--- consultar_cep.py
+++ consultar_cep.py
@@ -1,15 +1,10 @@
 import requests
 
 def main():
-	print('####################')
-	print('### Consulta CEP ###')
-	print('####################')
-	print()
-
-	cep_input = input('Digite o CEP para a consulta: ')
+	cep_input = input('\nDigite o CEP para a consulta: ')
 
 	if len(cep_input) != 8:
-		print('Quantidade de dígitos inválida!')
+		print('\n\033[0;31mQuantidade de dígitos inválida!\033[m\n')
 		exit()
 
 	request = requests.get('https://viacep.com.br/ws/{}/json/'.format(cep_input))
@@ -17,17 +12,17 @@ def main():
 	address_data = request.json()
 
 	if 'erro' not in address_data:
-		print('==> CEP ENCONTRADO <==')
+		print('\n\033[0;32mCEP ENCONTRADO\033[m\n')
 		
-		print('CEP: {}'.format(address_data['cep']))
-		print('Logradouro: {}'.format(address_data['logradouro']))
-		print('Complemento: {}'.format(address_data['complemento']))
-		print('Bairro: {}'.format(address_data['bairro']))
-		print('Cidade: {}'.format(address_data['localidade']))
-		print('Estado: {}'.format(address_data['uf']))
+		print('CEP: \033[0;33m{}\033[m\n'.format(address_data['cep']))
+		print('Logradouro: \033[0;34m{}\033[m'.format(address_data['logradouro']))
+		print('Complemento: \033[0;34m{}\033[m'.format(address_data['complemento']))
+		print('Bairro: \033[0;34m{}\033[m'.format(address_data['bairro']))
+		print('Cidade: \033[0;34m{}\033[m'.format(address_data['localidade']))
+		print('Estado: \033[0;34m{}\033[m'.format(address_data['uf']))
 		
 	else:
-		print('{}: CEP inválido.'.format(cep_input))
+		print('\033[0;31m{}: CEP inválido.\033[m'.format(cep_input))
 
 	print('---------------------------------')
 	option = int(input('Deseja realizar uma nova consulta ?\n1. Sim\n2. Sair\n'))
@@ -38,3 +33,4 @@ def main():
 
 if __name__ == '__main__':
 	main()
+print()
--- deseja_continuar.py
+++ deseja_continuar.py
@@ -15,5 +15,5 @@ while True:
     while resp not in "10":
         resp = str(input("\033[0;34mDeseja continuar [1 - SIM / 0 - NÃO]? \033[m")).strip().upper()[0]
     if resp == "0":
-        break    
+        break
 print("\033[0;36;1;4m\nVocê optou por finalizar!\033[m\n")
