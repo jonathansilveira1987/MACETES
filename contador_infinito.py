0483e8a027fa5a7e81d305a63a87f6d124b0a2cd
jonathansilveira1987 <jonathan.silveira@outlook.com>
2022-09-10 16:17:17 -0300

    Aprendizado

--- contador_infinito.py
+++ contador_infinito.py
@@ -8,12 +8,20 @@ while True:
         sleep(0.2)
         # print(count)
         print(f'\033[0;32m{count}\033[m')
+    # Contador com ponto Inicial & Final.
+    a = int(input('\nPonto A: '))
+    b = int(input('Ponto B: '))
+    c = range(a, b + 1)
+    print()
+    for d in c:
+            sleep(0.2)
+            print(f'\033[0;34m{d}\033[m')
     resp = " "
     while resp not in "10":
         resp = str(input("\nDeseja continuar contando manualmente [1 - SIM / 0 - NÃO]? ")).strip().upper()[0]
     if resp == "0":
         break
-print("\033[0;36;1;4m\nVocê optou por finalizar o contador manual! Segue contador automático...\033[m")
+print("\033[0;36;1;4m\nVocê optou por finalizar os contadores manuais! Segue contador automático...\033[m")
 # Contador Infinito Automático.
 i = 1
 print()
--- input_padrao.py
+++
@@ -1,7 +0,0 @@
-from time import sleep
-a = int(input('\nInício: '))
-b = int(input('Fim: '))
-c = range(a, b + 1)
-for d in c:
-		print(d)
-print()
