d6e6f5f5bf2913bc443fe514db9fe52f264d1fea
jonathansilveira1987 <jonathan.silveira@outlook.com>
2022-11-27 21:23:13 -0300

    Aprendizado

--- texto.py
+++
@@ -1,8 +0,0 @@
-texto = 'Título'
-
-print(f'\n{texto}')
-print(f'\n{texto: ^50}') # Centralizado.
-print(f'\n{texto:.^50}') # Centralizado.
-print(f'\n{texto:.<50}') # Alinhado a direita.
-print(f'\n{texto:.>50}') # Alinhado a esquerda.
-print()
