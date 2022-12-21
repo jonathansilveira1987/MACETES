e0e24af0157b3dd60204443d31b62948149e2f3e
jonathansilveira1987 <jonathan.silveira@outlook.com>
2022-12-21 00:12:47 -0300

    Aprendizado

---
+++ calcular_elevacao.py
@@ -0,0 +1,9 @@
+a = float(input('\nNúmero/Base: '))
+b = float(input('Potência/Expoente: '))
+resultado1 = a ** b
+print(f'\n{resultado1}')
+
+c = float(input('\nMontante: '))
+d = float(input('Taxa: '))
+resultado2 = c / d
+print(f'\n{resultado2}\n')
