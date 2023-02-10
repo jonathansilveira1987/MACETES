8ca6039d24d1c7d89353aa09ab7446d2b18d64aa
jonathansilveira1987 <jonathan.silveira@outlook.com>
2023-02-10 20:45:51 -0300

    Aprendizado

--- bateria_saude.py
+++ bateria_saude.py
@@ -3,6 +3,10 @@ import psutil
 # Captura o sensor da bateria
 battery = psutil.sensors_battery()
 # Captura o percentual da bateria
-percent = str(battery.percent)
-# Mostra o resultado
-print(f'\nNo momento você tem {percent}% de bateria.\n')
+percent = int(battery.percent)
+if percent <= 20:
+    # Mostra o resultado
+    print(f'\n\033[0;31mAtenção! Seu computador está operando com {percent}% de bateria. Conecte o carregador!\033[m\n')
+else:
+    # Mostra o resultado
+    print(f'\n\033[0;32mNo momento seu computador está operando com {percent}% de bateria.\033[m\n')
