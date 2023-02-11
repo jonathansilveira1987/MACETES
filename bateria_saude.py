4ae132b690179145d3a3d5c3ea4613e578446b93
jonathansilveira1987 <jonathan.silveira@outlook.com>
2023-02-10 22:08:29 -0300

    Aprendizado

--- bateria_saude.py
+++ bateria_saude.py
@@ -8,9 +8,11 @@ percent = int(battery.percent)
 if percent <= 20:
     # Mostra o resultado
     print(f'\n\033[0;31mAtenção! Seu computador está operando com {percent}% de bateria. Conecte o carregador!\033[m\n')
-    print(f'\033[0;31mEm 10 segundos será ativado o desligamento automático de seu computador para preservação da bateria.\033[m\n')
-    print(f'\033[0;31mCaso deseje cancelar o desligamento digite shutdown -a no Prompt de Comando e pressione ENTER.\033[m\n')
-    sleep(10)
+    sleep(5)
+    print(f'\033[0;35mEm 15 segundos será ativado o desligamento automático de seu computador para preservação da bateria.\033[m\n')
+    sleep(5)
+    print(f'\033[0;36mCaso deseje cancelar o desligamento digite shutdown -a no Prompt de Comando e pressione ENTER.\033[m\n')
+    sleep(15)
     import os
     os.system("shutdown /s /t 180")
 else:
