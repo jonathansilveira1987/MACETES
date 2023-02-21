db2f6efd18b83b32f2888a414ae84abbf57e2352
jonathansilveira1987 <jonathan.silveira@outlook.com>
2023-02-21 15:36:44 -0300

    Aprendizado

--- desenho.py
+++ desenho.py
@@ -8,8 +8,92 @@ import turtle
 
 
 
+
+
+
+
+
+
 '''
 
+# Triângulo - 3 lados.
+import turtle
+t = turtle.Turtle()
+for i in range(3):
+   t.forward(100) # Supondo que o lado de um triângulo é de 100 unidades
+   t.right(120) # Girando a tartaruga em 120 graus
+turtle.done()
+
+# Quadrilátero - 4 lados.
+import turtle
+t = turtle.Turtle()
+for i in range(4):
+   t.forward(100) # Supondo que o lado de um quadrilátero é de 100 unidades
+   t.right(90) # Girando a tartaruga em 90 graus
+turtle.done()
+
+# Pentágono - 5 lados.
+import turtle
+t = turtle.Turtle()
+for i in range(5):
+   t.forward(100) # Supondo que o lado de um pentágono é de 100 unidades
+   t.right(72) # Girando a tartaruga em 72 graus
+turtle.done()
+
+# Hexágono - 6 lados.
+import turtle
+t = turtle.Turtle()
+for i in range(6):
+   t.forward(100) # Supondo que o lado de um hexágono seja de 100 unidades
+   t.right(60) # Girando a tartaruga em 60 graus
+turtle.done()
+
+# Heptágono - 7 lados.
+import turtle
+t = turtle.Turtle()
+for i in range(7):
+   t.forward(100) # Supondo que o lado de um heptágono seja de 100 unidades
+   t.right(51.42) # Girando a tartaruga em 51,42 graus
+turtle.done()
+
+# Octógono - 8 lados.
+import turtle
+t = turtle.Turtle()
+for i in range(8):
+   t.forward(100) # Supondo que o lado de um octógono é de 100 unidades
+   t.right(45) # Girando a tartaruga em 45 graus
+turtle.done()
+
+# Polígono.
+import turtle 
+numberOfSides = int(input('\nInsira o número de lados de um polígono: '))
+lengthOfSide = int(input('Insira o comprimento de um lado de um polígono: '))
+exteriorAngle = 360/numberOfSides
+t = turtle.Turtle()
+for i in range(numberOfSides):
+   t.forward(lengthOfSide)
+   t.right(exteriorAngle)
+turtle.done()
+
+# Octógono - 8 lados.
+import turtle
+pen = turtle.Pen()   # Constrói um objeto caneta (Pen)
+pen.forward(100)     # anda 100 posições para a frente
+pen.right(45)        # gira o ponteiro em 45 graus para a direita
+pen.forward(100)     # anda mais 100 posições para a frente
+pen.right(45)        # gira o ponteiro em 45 graus para a direita
+pen.forward(100)     # anda mais 100 posições para a frente
+pen.right(45)        # gira o ponteiro em 45 graus para a direita
+pen.forward(100)     # anda mais 100 posições para a frente
+pen.right(45)        # gira o ponteiro em 45 graus para a direita
+pen.forward(100)     # anda mais 100 posições para a frente
+pen.right(45)        # gira o ponteiro em 45 graus para a direita
+pen.forward(100)     # anda mais 100 posições para a frente
+pen.right(45)        # gira o ponteiro em 45 graus para a direita
+pen.forward(100)     # anda mais 100 posições para a frente
+pen.right(45)        # gira o ponteiro em 45 graus para a direita
+pen.forward(100)     # anda mais 100 posições para a frente
+turtle.done()
 
 
 
@@ -46,25 +130,52 @@ import turtle
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Código Python para desenhar fractal "estrutura complexa cujas propriedades, em geral, repetem-se em qualquer escala" de flocos de neve.
+import turtle
+import random
+# Configurar a janela com uma cor de fundo.
+wn = turtle.Screen()
+wn.bgcolor("cyan")
+# Atribui um nome à sua tartaruga.
+elsa = turtle.Turtle()
+elsa.speed(15)
+# Criar uma lista de cores.
+sfcolor = ["white", "blue", "purple", "grey", "magenta"]
+# Criar uma função para criar flocos de neve de tamanhos diferentes.
+def snowflake(size):
+    # Move a caneta para a posição inicial.
+    elsa.penup()
+    elsa.forward(10*size)
+    elsa.left(45)
+    elsa.pendown()
+    elsa.color(random.choice(sfcolor))
+    # Desenha o galho 8 vezes para fazer um floco de neve.
+    for i in range(8):
+        branch(size)  
+        elsa.left(45)
+# criar um ramo do floco de neve.
+def branch(size):
+    for i in range(3):
+        for i in range(3):
+            elsa.forward(10.0*size/3)
+            elsa.backward(10.0*size/3)
+            elsa.right(45)
+        elsa.left(90)
+        elsa.backward(10.0*size/3)
+        elsa.left(45)
+    elsa.right(90)
+    elsa.forward(10.0*size)
+# Loop para criar 5 flocos de neve de tamanhos diferentes com terminais diferentes.
+for i in range(5):
+    x = random.randint(-200, 200)
+    y = random.randint(-200, 200)
+    sf_size = random.randint(1, 4)
+    elsa.penup()
+    elsa.goto(x, y)
+    elsa.pendown()
+    snowflake(sf_size)
+# Deixa a janela aberta até clicar para fechar.
+wn.exitonclick()
 
 
 
@@ -185,25 +296,7 @@ turtle.done()
 
 
 
-# Octógono
-import turtle
-pen = turtle.Pen()   # Constrói um objeto caneta  (Pen)
-pen.forward(100)     # anda 100 posições para a frente
-pen.right(45)        # gira o ponteiro em 45 graus para a direita
-pen.forward(100)     # anda mais 100 posições para a frente
-pen.right(45)        # gira o ponteiro em 45 graus para a direita
-pen.forward(100)     # anda mais 100 posições para a frente
-pen.right(45)        # gira o ponteiro em 45 graus para a direita
-pen.forward(100)     # anda mais 100 posições para a frente
-pen.right(45)        # gira o ponteiro em 45 graus para a direita
-pen.forward(100)     # anda mais 100 posições para a frente
-pen.right(45)        # gira o ponteiro em 45 graus para a direita
-pen.forward(100)     # anda mais 100 posições para a frente
-pen.right(45)        # gira o ponteiro em 45 graus para a direita
-pen.forward(100)     # anda mais 100 posições para a frente
-pen.right(45)        # gira o ponteiro em 45 graus para a direita
-pen.forward(100)     # anda mais 100 posições para a frente
-turtle.done()
+
 
 
 
