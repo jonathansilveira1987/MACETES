ef8679945ddf8eb4bf1f2fcfbab88d2066d2e9b0
jonathansilveira1987 <jonathan.silveira@outlook.com>
2023-03-07 22:18:15 -0300

    Codando

---
+++ JAVASCRIPT/Calculadora/script.js
@@ -0,0 +1,7 @@
+var valor1 = document.getElementById("valor1").value;
+var valor2 = document.getElementById("valor2").value;
+
+
+function result(){
+    document.getElementById("resultado").innerHTML = valor1 + valor2;
+}
