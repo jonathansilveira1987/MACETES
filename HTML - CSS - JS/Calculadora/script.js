381b29630780115d295b638041b6aeeb494bae3b
Jonathan <jonathan.silveira@outlook.com>
2023-03-17 19:58:47 -0300

    Codando

--- HTML - CSS - JS/Calculadora/script.js
+++
@@ -1,7 +0,0 @@
-var valor1 = document.getElementById("valor1").value;
-var valor2 = document.getElementById("valor2").value;
-
-
-function result(){
-    document.getElementById("resultado").innerHTML = valor1 + valor2;
-}
