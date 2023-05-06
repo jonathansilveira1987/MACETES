9f7b270551ba2709b1803feb283582178d989aa9
Jonathan <jonathan.silveira@outlook.com>
2023-05-06 13:24:08 -0300

    Codando

---
+++ PYTHON/analise_de_dados.py
@@ -0,0 +1,29 @@
+# a = 'Jonathan da Costa Silveira'
+a = input('\nDado: ')
+
+print(f'\nLower         > {a.lower()}')
+print(f'\nUpper         > {a.upper()}')
+print(f'\nCapitalize    > {a.capitalize()}')
+print(f'\nLen - Tamanho > {len(a)}')
+
+capital = 'A capital do Brasil é Buenos Aires.'
+capital_novo = capital.replace('Buenos Aires', 'Brasília')
+print(capital_novo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print()
