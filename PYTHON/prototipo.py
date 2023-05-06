692cbe0cc3b6be97c5cd17e220b0d2b75ce2f070
Jonathan <jonathan.silveira@outlook.com>
2023-05-06 16:19:35 -0300

    Codando

--- PYTHON/prototipo.py
+++ PYTHON/prototipo.py
@@ -1,30 +1,3 @@
-from time import sleep
-
-print('\nDisponibilizando ferramenta, por favor aguarde...')
-sleep(2)
-# Criar arquivo TXT.
-arquivo = open('bancodedados.txt', 'a')
-file = input('\nDigite algo: ')
-arquivo.write(file)
-# Lendo o arquivo criado:
-arquivo = open('bancodedados.txt','r')
-for linha in arquivo:
-    linha = linha.rstrip()
-    print (linha)
-arquivo.close()
-# Acrescentando texto ao arquivo criado, usando o modo de acesso 'a'
-print("\n")
-texto = input("Digite uma frase para acrescentar ao arquivo:\n")
-arquivo = open('arq01.txt','a')
-arquivo.write(texto + "\n")
-print("Operaçãoo concluída no arquivo " + arquivo.name + " usando o modo de acesso " + arquivo.mode)
-arquivo.close()
-print("\nTexto alterado:")
-arquivo = open('arq01.txt','r')
-for linha in arquivo:
-    linha = linha.rstrip()
-    print (linha)
-arquivo.close()
 # Acrescentando texto ao início do arquivo, usando o modo 'r+'
 print("\n")
 texto = input("Digite um titulo para acrescentar ao arquivo:\n")
