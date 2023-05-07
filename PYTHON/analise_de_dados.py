83b39a3e1ed892fbec64513cb0635b0baa8c3580
Jonathan <jonathan.silveira@outlook.com>
2023-05-07 12:39:40 -0300

    Codando

--- PYTHON/analise_de_dados.py
+++ PYTHON/analise_de_dados.py
@@ -1,11 +1,25 @@
-a = input('\nDado: ')
+string = 'Developer'
+print(f'''\n\033[0;36;1;3mPodemos fatiar uma String e acessar uma SubString com o recurso de Índice!\033[m
 
-print(f'\nLower         > {a.lower()}')
-print(f'\nUpper         > {a.upper()}')
-print(f'\nCapitalize    > {a.capitalize()}')
-print(f'\nLen - Tamanho > {len(a)} caracteres.')
+\033[0;31mString = {string}\033[m
+\033[0;32mString 1 dissecada = {string[1]}\033[m
+\033[0;33mString 5 dissecada = {string[5]}\033[m
+\033[0;34mString 8 dissecada = {string[8]}\033[m ''')
+
+a = input('\nDado: ')
+print(f'\nMétodo Lower         > {a.lower()}')
+print(f'\nMétodo Upper         > {a.upper()}')
+print(f'\nMétodo Capitalize    > {a.capitalize()}')
+print(f'\nMétodo Len - Tamanho > {len(a)} caracteres.')
 b = a.replace(a, 'Python Developer')
-print(f'\nReplace       > Substitui {a} por {b}.')
+print(f'\nMétodo Replace       > Substitui {a} por {b}.')
+
+
+
+
+
+
+
 
 
 
@@ -27,3 +41,13 @@ print(f'\nReplace       > Substitui {a} por {b}.')
 
 
 print()
+'''
+
+
+
+
+
+print(f'\Join           > {a.join()}')
+
+
+'''
