6b2f9633c4ec154909214a6436b47379ff8c8e8b
Jonathan <jonathan.silveira@outlook.com>
2023-05-13 21:05:42 -0300

    Codando

--- PYTHON/soma.py
+++ PYTHON/soma.py
@@ -5,7 +5,8 @@ while len(nums) < num:
     user_input = float(input("Valor: "))
     nums.append(user_input)
 listSum = sum(nums)
-print(f"\nA soma dos valores \033[0;33m{nums}\033[m = \033[0;34m{listSum:.2f}\033[m")
+print(f"\n\033[0;33mLista = {nums}\033[m")
+print(f"\n\033[0;34mSoma dos valores = {listSum:.2f}\033[m")
 
 def real(valor):
     a = "{:,.2f}".format(float(valor))
@@ -13,4 +14,4 @@ def real(valor):
     c = b.replace('.',',')
     return c.replace('v','.')
 # moeda = float(input('\nDigite o valor: '))
-print(f'\nA soma dos valores ficou em R$ {real(listSum)} reais.\n')
+print(f'\n\033[0;32mA soma dos valores ficou em R$ {real(listSum)} reais.\033[m\n')
