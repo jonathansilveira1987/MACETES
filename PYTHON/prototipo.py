b5407c34d0efa9763cb28caa1fb6209bc5296817
Jonathan <jonathan.silveira@outlook.com>
2023-05-19 23:08:30 -0300

    Codando

--- PYTHON/prototipo.py
+++ PYTHON/prototipo.py
@@ -1,13 +1,10 @@
-# ADICIONA MAIS DADOS EM UM MESMO ARQUIVO.
-nome = input('\nNome do Arquivo: ')
-elemento = int(input('\nInforme o número de elementos para a lista: '))
+from time import sleep
+
+# Contador Infinito.
+x = -1
 print()
-c = 0
-for i in range(c + 1):
-    conteudo = [input(f'{i + 1}º dado: ') for i in range(elemento)]
-    with open(nome + '.txt', 'a') as arquivo:
-        arquivo.write('\n'.join(conteudo))
-        arquivo.write(f'\nTotal de arquivos informados = {len(conteudo)}') # Informa total de arquivos.
-        arquivo.write('\n\n')
-    print(f'\nArquivo salvo com o nome de {nome}\n')
-    break
+while True:
+    x > 0
+    x = x + 1
+    sleep(0.3)
+    print(f'\033[32m{x}º\033[m')
