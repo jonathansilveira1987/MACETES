8bbadcd164c84e95626e73e081902545404e86a5
Jonathan <jonathan.silveira@outlook.com>
2023-05-21 17:46:12 -0300

    Codando

--- PYTHON/analise_exploratoria.py
+++ PYTHON/analise_exploratoria.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import pandas as pd
+import datetime as dt
 
 pnad = pd.read_csv("https://github.com/neylsoncrepalde/introducao_ao_r/blob/master/dados/pes_2012.csv?raw=true")
 print('\033[34m')
@@ -8,9 +10,10 @@ print('\033[34m')
 print(pnad)
 print()
 print(pnad['UF'])
-
-
-
+print()
+print(pnad.V0302)
+print()
+print(pnad.describe())
 
 
 
