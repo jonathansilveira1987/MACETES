509c43ff2860c07234760f6b42eaca830eb52898
Jonathan <jonathan.silveira@outlook.com>
2023-06-25 00:38:35 -0300

    Codando

---
+++ PYTHON/calcular_atmosfera.py
@@ -0,0 +1,6 @@
+atm = 101.325
+print(f'\n1 atm equivale a {atm}kg de peso por m2.')
+
+vetor = float(input('\nAtmosferas: '))
+resultado = vetor * atm
+print(f'\n{vetor} atmosfera(s) equivalem a {resultado}\n')
