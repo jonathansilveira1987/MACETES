b8415236ecd661690b35f2cb9017dd96f6fbb5cb
Jonathan <jonathan.silveira@outlook.com>
2023-06-25 13:00:33 -0300

    Codando

--- PYTHON/calcular_atmosfera.py
+++ PYTHON/calcular_atmosfera.py
@@ -1,6 +1,6 @@
 atm = 101.325
-print(f'\n1 atm equivale a {atm}kg de peso por m2.')
+print(f'\n1 atm equivale a {atm} Pa por sua vez, equivalente ao newton por metro quadrado (N/m²).')
 
 vetor = float(input('\nAtmosferas: '))
 resultado = vetor * atm
-print(f'\n{vetor} atmosfera(s) equivalem a {resultado}\n')
+print(f'\n{vetor:.0f} atmosfera(s) equivalem a {resultado:.3f} Pa\n')
