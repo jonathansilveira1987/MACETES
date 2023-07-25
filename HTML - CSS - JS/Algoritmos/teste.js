c6a33519ac223e77a5a440098fa72364edd409ef
Jonathan <jonathan.silveira@outlook.com>
2023-07-24 22:01:15 -0300

    Codando

--- HTML - CSS - JS/Algoritmos/teste.js
+++ HTML - CSS - JS/Algoritmos/teste.js
@@ -1,11 +1,56 @@
-let x = 1
-console.log(x)
-console.log("\nAqui tem um recado para você!\n");
+console.log("\n");
+// let x = 1
+// console.log(x)
+// console.log("\nAqui tem um recado para você!\n");
 
-var a = 'Jonathan ';
-var b = 'Silveira';
-var c = a + b;
-console.log(c);
+// var a = 'Jonathan ';
+// var b = 'Silveira';
+// var c = a + b;
+// console.log(c);
 
-var teste = 'Teste 1'
-console.log(teste);
+// var teste = 'Teste 1'
+// console.log(teste);
+
+// console.log(3 > 2)
+// console.log(3 < 2)
+// console.log('Jonathan' < 'Silveira')
+// console.log('Z' < 'a')
+// console.log('Jonathan' != 'Silveira')
+// console.log(NaN == NaN) /// NaN = Não há número.
+// console.log(true && false)
+// console.log(true && true)
+// console.log(false || true)
+// console.log(false || false)
+//console.log(1 + 1 == 2 && 10 * 10 > 50)
+// console.log(true ? 1 : 2);
+// console.log(false ? 1 : 2);
+// console.log(8 * null)
+//console.log('5' - 1)
+// console.log('5' + 1)
+// console.log('five' * 2)
+// console.log(false == 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
