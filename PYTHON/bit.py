e2eb963cd8eef5b3a53138f8c596d7841b07e635
Jonathan <jonathan.silveira@outlook.com>
2023-08-20 00:02:00 -0300

    Codando

--- PYTHON/bit.py
+++ PYTHON/bit.py
@@ -1,3 +1,17 @@
 gigabyte = float(input('\nGigabyte: '))
 convert = gigabyte * 1024
 print(f'\n{gigabyte} Gigabyte(s) equivale a {int(convert)} Megabytes.\n')
+
+n = int(input('Informe um número inteiro: '))
+quadrado = n * n 
+print(f'\nO quadrado de {n} é {quadrado}\n')
+
+s = 'Python Possibilities'
+print(len(s.partition('P')[0]))
+print()
+
+# n = 5
+for i in range(1, n+1):
+    print(' ' * (i-1), end='')
+    print('*' * (2*(n-i)+1))
+print()
