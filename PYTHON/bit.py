831209904c5073b5ce35555005e927e737c2327d
Jonathan <jonathan.silveira@outlook.com>
2023-08-23 21:49:41 -0300

    Codando

--- PYTHON/bit.py
+++ PYTHON/bit.py
@@ -15,3 +15,20 @@ for i in range(1, n+1):
     print(' ' * (i-1), end='')
     print('*' * (2*(n-i)+1))
 print()
+
+a = float(input('\nVetor A: '))
+b = float(input('Vetor B: '))
+
+if a < b:
+    c = a / b
+    d = c * 100
+    print(f'\n{a:.2f} equivale a {round(d)}% de {b:.2f}\n')
+else:
+    c = b / a
+    d = c * 100
+    print(f'\n{b:.2f} equivale a {c}% de {a:.2f}\n')
+
+
+# Por exemplo, se você quer calcular 35% de 500, 
+# multiplique 35 por 500. Fazendo isso você obtém o valor de 
+# 35 x 500 = 17500; Divida o resultado obtido por 100
