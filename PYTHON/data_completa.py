2293b01e4cd5c5c286d9699fd92bb8e528cfec08
Jonathan <jonathan.silveira@outlook.com>
2023-09-23 20:54:49 -0300

    Codando

---
+++ PYTHON/data_completa.py
@@ -0,0 +1,113 @@
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+
+
+
+
+
+
+import datetime
+dia = datetime.today().strftime('%d/%m')
+if dia == '22/09':
+    print('Feliz dia do programador!')
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Importa o módulo datetime.
+import datetime
+# Exibe a data atual.
+x = datetime.datetime.now()
+print(f'\n{x}')
+print(f'\n{x.strftime("%A")}') # Dia da semana
+print(f'\n{x.strftime("%w")}') # Número do dia da semana
+print(f'\n{x.strftime("%d")}') # Dia
+print(f'\n{x.strftime("%B")}') # Mês
+print(f'\n{x.strftime("%m")}') # Número do mês
+print(f'\n{x.strftime("%Y")}') # Ano
+print(f'\n{x.strftime("%H")}') # Hora
+print(f'\n{x.strftime("%p")}') # AM/PM
+print(f'\n{x.strftime("%M")}') # Minutos
+print(f'\n{x.strftime("%S")}') # Segundos
+print(f'\n{x.strftime("%f")}') # Microssegundo
+print(f'\n{x.strftime("%j")}') # Número do dia do ano
+print(f'\n{x.strftime("%U")}') # Número da semana do ano, domingo como primeiro dia da semana
+print(f'\n{x.strftime("%W")}') # Número da semana do ano, segunda-feira como o primeiro dia da semana
+print(f'\n{x.strftime("%c")}') # Versão local de data e hora
+print(f'\n{x.strftime("%C")}') # Século
+print(f'\n{x.strftime("%X")}') # Versão local da hora
+print(f'\n{x.strftime("%G")}') # Ano ISO 8601
+print(f'\n{x.strftime("%u")}') # Dia da semana ISO 8601
+print(f'\n{x.strftime("%V")}') # Número da semana ISO 8601
+
+
+
+
+'''
---
+++ PYTHON/dia_do_programador.py
@@ -0,0 +1,10 @@
+# Importa o módulo datetime.
+import datetime
+# Exibe a data atual.
+x = datetime.datetime.now()
+print(f'\nHoje é o {x.strftime("%j")}º dia do ano.') # Número do dia do ano
+dia = x.strftime("%d")
+mes = x.strftime("%m")
+if dia == '13' and mes == '09':
+    print('\nFeliz dia do programador!')
+print()
