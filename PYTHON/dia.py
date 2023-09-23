dd815fdbb14f0704a1a90ae4df0baf5663d4b2b7
Jonathan <jonathan.silveira@outlook.com>
2023-09-23 00:00:55 -0300

    Codando

---
+++ PYTHON/dia.py
@@ -0,0 +1,26 @@
+import datetime
+
+x = datetime.datetime.now()
+print(f'\n{x}')
+
+
+
+
+
+
+
+
+
+
+'''
+
+
+import datetime
+
+dia = datetime.today().strftime('%d/%m')
+
+if dia == '22/09':
+    print('Feliz dia do programador!')
+
+
+'''
