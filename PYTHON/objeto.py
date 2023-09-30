c94e027aa431b6f2c321465fd68677e0cd6bbe57
Jonathan <jonathan.silveira@outlook.com>
2023-09-30 13:02:16 -0300

    Codando

--- PYTHON/objeto.py
+++ PYTHON/objeto.py
@@ -1,35 +1,11 @@
 # Calculadora.
 def menu():
-    a = int(input('\nValor 1: '))
-    b = int(input('Valor 2: '))
-
-    print('''
-Escolha a operação desejada:
-+
--
-*
-/
-    ''')
-    operacao = input('> ')
-
-    if operacao == '+':
-        result1 = a + b
+    def imprimir_padrao():
+        tamanho = int(input('\nTamanho do Quadrado: '))
         print()
-        print(a, '+', b, '=', result1)
-        print('\nO resultado de', a, '+', b, 'é', result1)
-        print(f'O resultado de {a} + {b} é {result1}')
-    elif operacao == '-':
-        result2 = a - b
-        print(f'\nO resultado de {a} - {b} é {result2}')
-    elif operacao == '*':
-        result3 = a * b
-        print(f'\nO resultado de {a} * {b} é {result3}')
-    elif operacao == '/':
-        result4 = a / b
-        print(f'\nO resultado de {a} / {b} é {result4}')
-    else:
-        operacao != '+, -, *, /'
-        print('\nVocê digitou uma operação inválida!')
-        # print('A calculadora foi encerrada!')
+        for i in range(tamanho):
+            print('\033[0;32m ■ \033[m' * tamanho)
+    imprimir_padrao()
+    # print()
 while True:
     menu()
--- PYTHON/salario_detalhado.py
+++ PYTHON/salario_detalhado.py
@@ -1,16 +1,16 @@
 # Salário.
-valor = float(input('\nInforme um valor: '))
-dias_trabalhados = int(input('Dias trabalhados: '))
-carga_horaria = int(input('Horas trabalhadas ao dia: '))
+valor = float(input('\nInforme o valor do salário R$ '))
+dias_trabalhados = int(input('Informe os dias trabalhados: '))
+carga_horaria = int(input('Infomre as horas trabalhadas ao dia: '))
 valor_real = "\033[0;32mR$ {:,.2f}\033[m".format(valor).replace(",", "X").replace(".", ",").replace("X", ".")
 print(f'\nSalário: {valor_real}')
 
 # Valor por dia.
 valor_diario = valor / dias_trabalhados
 valor_real = "\033[0;33mR$ {:,.2f}\033[m".format(valor_diario).replace(",", "X").replace(".", ",").replace("X", ".")
-print(f'\nValor Diário: {valor_real}')
+print(f'\nValor recebido diariamente: {valor_real}')
 
 # Valor por hora.
 valor_hora = valor_diario / carga_horaria
 valor_real = "\033[0;34mR$ {:,.2f}\033[m".format(valor_hora).replace(",", "X").replace(".", ",").replace("X", ".")
-print(f'\nValor Hora: {valor_real}\n')
+print(f'\nValor por hora: {valor_real}\n')
