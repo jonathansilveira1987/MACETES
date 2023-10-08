a99ca65cdc4a01e41fdc10b80b7c800da4aae4df
Jonathan <jonathan.silveira@outlook.com>
2023-10-08 20:50:19 -0300

    Codando

--- PYTHON/prototipo.py
+++ PYTHON/prototipo.py
@@ -1,3 +1,22 @@
+name = 'Jonathan'
+idade = 36
+frase = f'{name} tem {idade} anos de idade'
+print(frase)
+
+precos = {
+    '\nCelular': 1500.00,
+    '\nComputador': 5000.00,
+    '\nTablet': 2500.00,
+    }
+for item in precos:
+    print(f'{item}')
+    print(f'{precos[item]}')
+
+# numero = 1500
+numero = int(input('\nNúmero: '))
+numero = f'R$ {numero:,.2f}'
+print(f'\n{numero}\n')
+
 def menu():
     ano = int(input('\nDigite o ano desejado: '))
     if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
