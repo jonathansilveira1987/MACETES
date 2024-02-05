b974fa9142d6dd091d03757fc2515cc7d53a5470
Jonathan <jonathan.silveira@outlook.com>
2024-02-05 20:45:43 -0300

    Codando

---
+++ PYTHON/010.py
@@ -0,0 +1,9 @@
+a = int(input('\nDigitar A: '))
+b = int(input('Digitar B: '))
+c = int(input('Digitar C: '))
+print(f'\n{a , b, c}\n')
+
+
+valores = input('Digite 3 valores separados: ').split()
+a, b, c = map(int, valores)
+print(f'\n{a , b, c}\n')
