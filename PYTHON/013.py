188ab846eabeb719df6239e07a71f69608c183fc
Jonathan <jonathan.silveira@outlook.com>
2024-04-07 20:51:34 -0300

    Codando

--- PYTHON/013.py
+++ PYTHON/013.py
@@ -1,19 +1,32 @@
-
-
-lista = [1 ,2, 3, 4, 5]
-
-print(lista)
-
-print(lista.count())
-
-resultado1 = lista.count()
-print(resultado1)
-
-
-
-
+lista = [1 ,2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(f'\nLista atual: {lista}')
+remover = int(input('Qual elemento da lista acima você deseja remover? '))
+elemento_removido = lista.remove(remover)
+print(f'\nElemento removido: {remover}')
+print(f'\nLista atualizada: {lista}\n')
 
 '''
+
+
+elemento = int(input('\nInforme o número de elementos para a lista: '))
+print()
+lista = [input("Dado: ") for i in range(elemento)]
+numeros = lista
+print()
+print(f'\n{numeros}')
+remover = input('Qual elemento da lista acima você deseja remover? ')
+elemento_removido = numeros.remove(remover)
+print(f'\nElemento removido: {remover}')
+print(f'\nLista atualizada: {numeros}\n')
+
+
+
+
+
+
+
+
+
 
 a = int(input('\nValor 1: '))
 b = int(input('Valor 2: '))
