bd202f7898334f1d393f4b4c10f37e727956c18b
Jonathan <jonathan.silveira@outlook.com>
2025-05-18 19:51:32 -0300

    Codando

--- PYTHON/005.py
+++
@@ -1,5 +0,0 @@
-texto = str(input('\nTexto: '))
-
-print(f'\n{texto:.^50}')
-print(f'\n{texto:.>50}')
-print(f'\n{texto:.<50}\n')
--- PYTHON/007.py
+++
@@ -1,5 +0,0 @@
-n = 9
-print()
-for i in range(1, n + 1):
-    print(((10 ** i - 1) // 9) ** 2)
-print()
