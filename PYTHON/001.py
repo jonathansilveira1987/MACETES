65e85142cb64d602a83604d079ab338a98a2f3eb
Jonathan <jonathan.silveira@outlook.com>
2025-05-18 15:33:01 -0300

    Codando

--- PYTHON/001.py
+++ PYTHON/001.py
@@ -1,32 +1,5 @@
-import calendar
-
-ano = int(input('\nAno: '))
-mes = int(input('Mês: '))
-print(f'\n{calendar.month(ano, mes)}')
-
-
-year = ano
-print(f'\n{calendar.calendar(year)}')
-
-import turtle
-import colorsys
-
-p = turtle.Turtle()
-s = turtle.Screen()
-s.title("Estude!")
-s.bgcolor("black")
-p.pensize(1.5)
-p.speed(0)
-n = 36
-h = 0.5
-
-
-for i in range(75):
-    c = colorsys.hsv_to_rgb(h, 1, 1)
-    h += 1 / n
-    p.color(c)
-    p.left(5)
-    for j in range(5):
-        p.forward(250)
-        p.left(144)
-turtle.done()
+a = int(input('Valor: '))
+if a == type(str):
+    print(a)
+else:
+    print('Não foi digitado valor numérico, por favor, tente novamente!')
--- PYTHON/015.py
+++ PYTHON/015.py
@@ -7,3 +7,36 @@ salario_antigo = float(input("\nSalário Antigo: R$ "))
 salario_novo = float(input("Novo Salário: R$ "))
 novo_salario = ((salario_novo - salario_antigo) / salario_antigo) * 100
 print("\nO valor de reajuste foi de \033[0;32m{:.2f}%\033[m.\n".format(novo_salario))
+
+import calendar
+
+ano = int(input('\nAno: '))
+mes = int(input('Mês: '))
+print(f'\n{calendar.month(ano, mes)}')
+
+
+year = ano
+print(f'\n{calendar.calendar(year)}')
+
+import turtle
+import colorsys
+
+p = turtle.Turtle()
+s = turtle.Screen()
+s.title("Estude!")
+s.bgcolor("black")
+p.pensize(1.5)
+p.speed(0)
+n = 36
+h = 0.5
+
+
+for i in range(75):
+    c = colorsys.hsv_to_rgb(h, 1, 1)
+    h += 1 / n
+    p.color(c)
+    p.left(5)
+    for j in range(5):
+        p.forward(250)
+        p.left(144)
+turtle.done()
